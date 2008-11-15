9670a000fa9ed421011e024d53c8f83b0a3a2db1
unknown <okomok@gmail.com>
2008-11-16 07:05:41 +0900

    better Expr

--- src/main/scala/mada/Expr.scala
+++ src/main/scala/mada/Expr.scala
@@ -2,34 +2,61 @@
 package mada
 
 
+// Expr
+
 object Expr {
-    def apply[A](e: => A) = new Expression(e).expr
+    def apply[A](e: => A) = new ConstantExpr(e).expr
 }
 
 trait Expr[A] {
-    protected def _eval: A
-    protected def _eval[B](c: Context[A, B]): B = c(this)
-    final def eval = _eval
-    final def eval[B](c: Context[A, B]) = _eval(c)
+    def _eval: A = { throw new ErrorUnknownContext(this, new DefaultContext[A]) }
+    def _eval[B](c: Context[A, B]): B = c(this)
+
+    final def eval: A = eval(new DefaultContext[A])
+    final def eval[B](c: Context[A, B]): B = _eval(c)
+
     final def expr: Expr[A] = this
     final def toLazy[A] = LazyExpr(this).expr
 }
 
+
+// contexts
+
 trait Context[A, B] extends (Expr[A] => B)
 
+case class DefaultContext[A] extends Context[A, A] {
+    override def apply(x: Expr[A]) = x._eval
+}
 
-case class LazyExpr[A](_1: Expr[A]) extends Expr[A] {
-    override lazy val _eval = _1.eval
+case class LazyContext[A, B](c: Context[A, B]) extends Context[A, B] {
+    override def apply(x: Expr[A]) = { lazy val v = c(x); v }
 }
 
 
-class Expression[A](e: => A) extends Expr[A] {
+// expressions
+
+class ConstantExpr[A](e: => A) extends Expr[A] {
     override def _eval = e
 }
 
+case class LazyExpr[A](_1: Expr[A]) extends Expr[A] {
+    override def _eval = _1.eval
+    override def _eval[B](c: Context[A, B]) = _1.eval(new LazyContext(c))
+}
+
+case class ForwardExpr[A](_1: Expr[A]) extends Expr[A] {
+    override def _eval = _1.eval
+    override def _eval[B](c: Context[A, B]) = _1.eval(c)
+}
 
 object ExprConversions extends ExprConversions
 
 trait ExprConversions {
     implicit def toMadaExpr[A](e: => A) = Expr(e)
 }
+
+
+// exceptions
+
+case class ErrorUnknownExpr[A, B](expr: Expr[A], context: Context[A, B]) extends UnsupportedOperationException
+case class ErrorUnknownContext[A, B](expr: Expr[A], context: Context[A, B]) extends UnsupportedOperationException
