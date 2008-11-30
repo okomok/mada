34cb6f885d622ab1433d82f2b08431aec4ce410a
unknown <okomok@gmail.com>
2008-11-30 11:30:05 +0900

    much better test

--- src/test/scala/madatest/rng/detail/TestSinglePass.scala
+++ src/test/scala/madatest/rng/detail/TestSinglePass.scala
@@ -5,7 +5,7 @@ package madatest.rng.detail
 import mada.Implies._
 import mada.rng._
 import mada.rng.From._
-import mada.rng.Pointer._
+import mada.rng.ToArray._
 import junit.framework.Assert._
 
 
@@ -20,33 +20,8 @@ object TestSinglePassReadOnly {
         assertTrue(!(p != q) implies p == q)
 
         if (actual models ForwardTraversal)
-            assertEquals(from(expected).eval, p <=< q) // assertEquals is a multi-pass algorithm.
+            assertEquals(from(expected).eval, p <=< q)
         else
-            TestSinglePassEquality(from(expected).eval, p <=< q)
-    }
-}
-
-
-object TestSinglePassEquality {
-    def apply[A](expected: Rng[A], actual: Rng[A]) {
-        val (p, q) = actual.toPair
-        val (ep, eq) = expected.toPair
-        while (p != q && ep != eq) {
-            assertEquals("elements not equal", *(p), *(ep))
-            ++(p); ++(ep)
-        }
-
-        assertEquals("actual rng is shorter than expected.", ep, eq);
-        assertEquals("actual rng is longer than expected.", p, q);
-    }
-}
-
-
-class SiglePassEqualityTest {
-    def testTrivial {
-        val r1 = from(Array(0, 0, 4, 4, 6, 8,11,12,13,14,15,17,18,19,23)).eval
-        val r2 = from(Array(0, 0, 4, 4, 6, 8,11,12,13,14,15,17,18,19,23)).eval
-        assertNotSame(r1, r2)
-        TestSinglePassEquality(r1, r2)
+            assertEquals(from(expected).eval, (p <=< q).toExpr.rng_toArray.eval)
     }
 }
