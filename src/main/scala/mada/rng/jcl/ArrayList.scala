32d543c2ec4efb29f8a2ba42f9bd8b66f92bf7f3
unknown <okomok@gmail.com>
2008-12-06 09:36:12 +0900

    no msg

--- src/main/scala/mada/rng/jcl/ArrayList.scala
+++ src/main/scala/mada/rng/jcl/ArrayList.scala
@@ -5,7 +5,7 @@ package mada.rng.jcl
 // ArrayList[A] <-> Expr[Rng[A]]
 
 object ArrayListCompatible extends ArrayListCompatible; trait ArrayListCompatible {
-    implicit def madaRngJcl_ArrayList2RngExpr[A](from: java.util.ArrayList[A]): Expr[Rng[A]] = FromArrayListExpr(Expr(from)).expr
+    implicit def madaRngJcl_ArrayList2ExprRng[A](from: java.util.ArrayList[A]): Expr[Rng[A]] = FromArrayListExpr(Expr(from)).expr
 }
 
 
