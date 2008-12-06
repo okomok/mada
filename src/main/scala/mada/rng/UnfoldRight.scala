65359131d91621ab6a649cbed42cba4aa2aceb04
unknown <okomok@gmail.com>
2008-12-06 11:29:29 +0900

    Adds UnfoldRight

---
+++ src/main/scala/mada/rng/UnfoldRight.scala
@@ -0,0 +1,28 @@
+
+package mada.rng
+
+
+object UnfoldRight extends UnfoldRight; trait UnfoldRight extends Predefs {
+    final def unfoldRight[From, To](_1: From, _2: From => Option[(To, From)]) = UnfoldRightExpr(_1, _2).expr
+}
+
+
+case class UnfoldRightExpr[From, To](_1: From, _2: From => Option[(To, From)]) extends Expr[Rng[To]] {
+    override def _eval = UnfoldRightImpl(_1, _2)
+}
+
+
+object UnfoldRightImpl {
+    def apply[From, To](z: From, f: From => Option[(To, From)]): Rng[To] = {
+        new UnfoldRightPointer(f(z), f) <=< new UnfoldRightPointer(None, f)
+    }
+}
+
+// ForwardTraversal would need state is copyable.
+class UnfoldRightPointer[From, To](var state: Option[(To, From)], val function: From => Option[(To, From)])
+        extends PointerFacade[To, UnfoldRightPointer[From, To]] {
+    override def _read = state.get._1
+    override def _traversal = SinglePassTraversal
+    override def _equals(that: UnfoldRightPointer[From, To]) = state.isEmpty == that.state.isEmpty
+    override def _increment = { state = function(state.get._2) }
+}
---
+++ src/test/scala/madatest/rng/UnfoldRightTest.scala
@@ -0,0 +1,31 @@
+
+package madatest.rng
+
+
+import detail.Example._
+import mada.rng.From._
+import mada.rng.ToRng._
+import mada.rng.UnfoldRight._
+import junit.framework.Assert._
+
+
+/*
+unfoldr      :: (b -> Maybe (a, b)) -> b -> [a]
+unfoldr f b  =
+  case f b of
+   Just (a,new_b) -> a : unfoldr f new_b
+   Nothing        -> []
+*/
+
+
+class UnfoldRightTest {
+    def testTrivial: Unit = {
+        val x = unfoldRight(5, { (b: Int) => if (b == 0) None else Some(b, b-1) })
+        detail.TestSinglePassReadOnly(Array(5,4,3,2,1), x.eval)
+    }
+
+    def testEmpty: Unit = {
+        val x = unfoldRight(5, { (b: Int) => None })
+        detail.TestEmpty(x.eval)
+    }
+}
