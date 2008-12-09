8052d31edf7458fa0d4cec45d50cb7438337eb97
unknown <okomok@gmail.com>
2008-12-10 08:41:43 +0900

    no msg

--- src/test/scala/madatest/rng/FlattenTest.scala
+++ src/test/scala/madatest/rng/FlattenTest.scala
@@ -5,6 +5,7 @@ package madatest.rng
 import mada.NDebug
 import mada.rng._
 import mada.rng.Flatten._
+import mada.rng.AsRngBy._
 import mada.rng.From._
 import junit.framework.Assert._
 import detail.Example._
@@ -17,6 +18,7 @@ class FlattenTest {
         val r3 = from(Array(6, 4,23, 0,12,15)).eval
         val r4 = from(Array(11, 4)).eval
         val actual = from(Array[Rng[Int]](r1, r2, r3, r4)).flatten(RandomAccessTraversal).eval
+        assertEquals(BidirectionalTraversal, actual.traversal)
         detail.TestBidirectionalReadWrite(example1, actual)
     }
 
@@ -27,6 +29,7 @@ class FlattenTest {
         val r3 = from(Array(6, 4,23, 0,12,15)).eval
         val r4 = from(Array(11, 4)).eval
         val actual = from(Array[Rng[Int]](re, r1, re, re, re, r2, r3, re, r4, re)).flatten(RandomAccessTraversal).eval
+        assertEquals(BidirectionalTraversal, actual.traversal)
         detail.TestBidirectionalReadWrite(example1, actual)
     }
 
@@ -36,11 +39,22 @@ class FlattenTest {
         val r3 = from(Array(6, 4,23, 0,12,15)).eval
         val r4 = from(Array(11, 4)).eval
         val actual = from(Array[Rng[Int]](r1, r2, r3, r4)).flatten.eval
+        assertEquals(SinglePassTraversal, actual.traversal)
         detail.TestSinglePassReadOnly(example1, actual)
     }
 
     def testEmpty {
         val r = from(empty1).eval
         detail.TestEmpty(from(Array[Rng[Int]](r, from(empty1).eval, r)).flatten.eval)
+    }
+
+    def testMistery {
+        val r1 = from(Array(0,18,14,17)).eval
+        val r2 = from(Array(19, 8,13)).eval
+        val r3 = from(Array(6, 4,23, 0,12,15)).eval
+        val r4 = from(Array(11, 4)).eval
+        val actual = from(Array[Rng[Int]](r1, r2, r3, r4)).asRngBy(SinglePassTraversal).flatten(RandomAccessTraversal).eval
+        assertEquals(SinglePassTraversal, actual.traversal)
+        detail.TestSinglePassReadOnly(example1, actual)
     }
 }
