7c45bc6651f24218485390426631ae0eff2d8ecb
unknown <okomok@gmail.com>
2008-12-10 21:38:08 +0900

    merge draft

---
+++ src/main/scala/mada/rng/Rotate.scala
@@ -0,0 +1,27 @@
+
+package mada.rng
+
+
+import Append._
+
+
+object Rotate extends Rotate; trait Rotate extends Predefs {
+    class MadaRngRotate[A](_1: Expr.Of[Rng[A]]) {
+        def rotate(_2: Rng[A] => Pointer[A]) = RotateExpr(_1, _2).expr
+    }
+    implicit def toMadaRngRotate[A](_1: Expr.Of[Rng[A]]): MadaRngRotate[A] = new MadaRngRotate[A](_1)
+}
+
+
+case class RotateExpr[A](override val _1: Expr.Of[Rng[A]], _2: Rng[A] => Pointer[A]) extends Expr.Transform[Rng[A]] {
+    override protected def _default = RotateImpl(_1.eval, _2)
+}
+
+
+object RotateImpl {
+    def apply[A](r: Rng[A], f: Rng[A] => Pointer[A]): Rng[A] = {
+        AssertModels(r, ForwardTraversal)
+        val mid = f(r)
+        (mid <=< r.end)./.append(r.begin <=< mid)./
+    }
+}
