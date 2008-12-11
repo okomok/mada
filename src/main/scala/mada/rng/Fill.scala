d4b30b34fbed37bdfdfb993ea9b46d4ebebc5c3a
unknown <okomok@gmail.com>
2008-12-12 07:20:49 +0900

    Adds replace

--- src/main/scala/mada/rng/Fill.scala
+++ src/main/scala/mada/rng/Fill.scala
@@ -2,9 +2,6 @@
 package mada.rng
 
 
-import Pointer._
-
-
 object Fill extends Fill; trait Fill extends Predefs {
     class MadaRngFill[A](_1: Expr.Of[Rng[A]]) {
         def fill[B <: A](_2: B) = FillExpr(_1, _2).expr
@@ -14,5 +11,5 @@ object Fill extends Fill; trait Fill extends Predefs {
 
 
 case class FillExpr[A, B <: A](_1: Expr.Of[Rng[A]], _2: B) extends Expr.Alias[Rng[A], Unit] {
-    override protected def _alias = ForeachExpr(OutdirectExpr(_1), *(_: Pointer[A]) = _2)
+    override protected def _alias = ReplaceExpr(_1, { (e: A) => _2 })
 }
---
+++ src/main/scala/mada/rng/Replace.scala
@@ -0,0 +1,18 @@
+
+package mada.rng
+
+
+import Pointer._
+
+
+object Replace extends Replace; trait Replace extends Predefs {
+    class MadaRngReplace[A](_1: Expr.Of[Rng[A]]) {
+        def replace(_2: A => A) = ReplaceExpr(_1, _2).expr
+    }
+    implicit def toMadaRngReplace[A](_1: Expr.Of[Rng[A]]): MadaRngReplace[A] = new MadaRngReplace[A](_1)
+}
+
+
+case class ReplaceExpr[A](_1: Expr.Of[Rng[A]], _2: A => A) extends Expr.Alias[Rng[A], Unit] {
+    override protected def _alias = ForeachExpr(OutdirectExpr(_1), { (p: Pointer[A]) => *(p) = _2(*(p)) })
+}
