5b0ab72025c2848af91a663eff264a44987b11cc
unknown <okomok@gmail.com>
2008-12-12 00:12:20 +0900

    nomsg

--- src/main/scala/mada/rng/LeftsRights.scala
+++ src/main/scala/mada/rng/LeftsRights.scala
@@ -5,26 +5,26 @@ package mada.rng
 // lefts
 
 object Lefts extends Lefts; trait Lefts extends Predefs {
-    class MadaRngLefts[A1, A2](_1: Expr.Of[Rng[Either[A1, A2]]]) {
+    class MadaRngLefts[A, B](_1: Expr.Of[Rng[Either[A, B]]]) {
         def lefts = LeftsExpr(_1).expr
     }
-    implicit def toMadaRngLefts[A1, A2](_1: Expr.Of[Rng[Either[A1, A2]]]): MadaRngLefts[A1, A2] = new MadaRngLefts[A1, A2](_1)
+    implicit def toMadaRngLefts[A, B](_1: Expr.Of[Rng[Either[A, B]]]): MadaRngLefts[A, B] = new MadaRngLefts[A, B](_1)
 }
 
-case class LeftsExpr[A1, A2](_1: Expr.Of[Rng[Either[A1, A2]]]) extends Expr.Alias[Rng[Either[A1, A2]], Rng[A1]] {
-    override protected def _alias = MapExpr(FilterExpr(_1, (_: Either[A1, A2]).isLeft), (_: Either[A1, A2]).left.get)
+case class LeftsExpr[A, B](_1: Expr.Of[Rng[Either[A, B]]]) extends Expr.Alias[Rng[Either[A, B]], Rng[A]] {
+    override protected def _alias = MapExpr(FilterExpr(_1, (_: Either[A, B]).isLeft), (_: Either[A, B]).left.get)
 }
 
 
 // rights
 
 object Rights extends Rights; trait Rights extends Predefs {
-    class MadaRngRights[A1, A2](_1: Expr.Of[Rng[Either[A1, A2]]]) {
+    class MadaRngRights[A, B](_1: Expr.Of[Rng[Either[A, B]]]) {
         def rights = RightsExpr(_1).expr
     }
-    implicit def toMadaRngRights[A1, A2](_1: Expr.Of[Rng[Either[A1, A2]]]): MadaRngRights[A1, A2] = new MadaRngRights[A1, A2](_1)
+    implicit def toMadaRngRights[A, B](_1: Expr.Of[Rng[Either[A, B]]]): MadaRngRights[A, B] = new MadaRngRights[A, B](_1)
 }
 
-case class RightsExpr[A1, A2](_1: Expr.Of[Rng[Either[A1, A2]]]) extends Expr.Alias[Rng[Either[A1, A2]], Rng[A2]] {
-    override protected def _alias = MapExpr(FilterExpr(_1, (_: Either[A1, A2]).isRight), (_: Either[A1, A2]).right.get)
+case class RightsExpr[A, B](_1: Expr.Of[Rng[Either[A, B]]]) extends Expr.Alias[Rng[Either[A, B]], Rng[B]] {
+    override protected def _alias = MapExpr(FilterExpr(_1, (_: Either[A, B]).isRight), (_: Either[A, B]).right.get)
 }
