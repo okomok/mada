f83dbbc16f528cc68c6890275d497a24db8411f4
unknown <okomok@gmail.com>
2008-12-13 00:45:17 +0900

    Moves to stlport

--- src/main/scala/mada/rng/detail/Median.scala
+++ src/main/scala/mada/rng/detail/Median.scala
@@ -4,34 +4,33 @@
 // Distributed under the terms of an MIT-style license.
 
 
-package mada.rng.detail
-
-
 /*
  *
  * Copyright (c) 1994
  * Hewlett-Packard Company
  *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Hewlett-Packard Company makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
- *
- *
- * Copyright (c) 1996
+ * Copyright (c) 1996,1997
  * Silicon Graphics Computer Systems, Inc.
  *
- * Permission to use, copy, modify, distribute and sell this software
- * and its documentation for any purpose is hereby granted without fee,
- * provided that the above copyright notice appear in all copies and
- * that both that copyright notice and this permission notice appear
- * in supporting documentation.  Silicon Graphics makes no
- * representations about the suitability of this software for any
- * purpose.  It is provided "as is" without express or implied warranty.
+ * Copyright (c) 1997
+ * Moscow Center for SPARC Technology
+ *
+ * Copyright (c) 1999
+ * Boris Fomitchev
+ *
+ * This material is provided "as is", with absolutely no warranty expressed
+ * or implied. Any use is at your own risk.
+ *
+ * Permission to use or copy this software for any purpose is hereby granted
+ * without fee, provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
+ *
  */
+
+
+package mada.rng.detail
 
 
 object Median {
