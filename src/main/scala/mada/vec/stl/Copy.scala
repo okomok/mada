6e9fb3ba80a52e328ad96bc96c3c8030a989aff2
unknown <okomok@gmail.com>
2008-12-18 08:40:52 +0900

    nomsg

--- src/main/scala/mada/vec/stl/Copy.scala
+++ src/main/scala/mada/vec/stl/Copy.scala
@@ -9,28 +9,27 @@ package mada.vec.stl
 
 object Copy extends Copy; trait Copy extends Predefs {
     class MadaVecStlCopy[From](_1: Expr.Of[Vector[From]]) {
-        def stl_copy[To >: From](_2: Expr.Of[Vector.Pointer[From]]) = CopyExpr(_1, _2).expr
-        def stl_copyIf[To >: From](_2: Expr.Of[Vector.Pointer[From]], _3: From => Boolean) = CopyIfExpr(_1, _2, _3).expr
+        def stl_copy[To >: From](_2: Vector.Pointer[To]) = CopyExpr(_1, _2).expr
+        def stl_copyIf[To >: From](_2: Vector.Pointer[To], _3: From => Boolean) = CopyIfExpr(_1, _2, _3).expr
     }
     implicit def toMadaVecStlCopy[From](_1: Expr.Of[Vector[From]]): MadaVecStlCopy[From] = new MadaVecStlCopy[From](_1)
 }
 
 
-case class CopyExpr[From, To >: From](override val _1: Expr.Of[Vector[From]], _2: Expr.Of[Vector.Pointer[To]])
+case class CopyExpr[From, To >: From](override val _1: Expr.Of[Vector[From]], _2: Vector.Pointer[To])
 extends Expr.Method[Vector[From], Vector.Pointer[To]] {
     override protected def _default = {
-        var __result = _2.eval
-        ForEachExpr[From](_1, { e: From => __result.write(e); __result += 1 }).eval // enables fusion.
-        __result
+        ForEachExpr[From](_1, { e: From => _2.write(e); _2 += 1 }).eval // enables fusion.
+        _2
     }
 }
 
 
-case class CopyIfExpr[From, To >: From](_1: Expr.Of[Vector[From]], _2: Expr.Of[Vector.Pointer[To]], _3: From => Boolean)
+case class CopyIfExpr[From, To >: From](_1: Expr.Of[Vector[From]], _2: Vector.Pointer[To], _3: From => Boolean)
 extends Expr.Method[Vector[From], Vector.Pointer[To]] {
     override protected def _default = {
         val (__*, __first, __last) = _1.eval.toTriple
-        CopyIfImpl(__*, __first, __last, _2.eval, _3)
+        CopyIfImpl(__*, __first, __last, _2, _3)
     }
 }
 
@@ -42,9 +41,9 @@ object CopyIfImpl {
         while (__n > 0) {
             if (__pred(*(__first))) {
                 *(__result) = *(__first)
+                __result += 1
             }
             __first += 1
-            __result += 1
             __n -= 1
         }
         return __result
