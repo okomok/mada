74bc77f24c2041200279fed56f68f3cf2104ec8a
unknown <okomok@gmail.com>
2008-12-30 05:39:30 +0900

    nomsg

--- src/main/scala/mada/vec/jcl/ListIterator.scala
+++ src/main/scala/mada/vec/jcl/ListIterator.scala
@@ -17,12 +17,30 @@ object VectorListIterator {
 class VectorListIterator[A](* : Vector[A]) extends ListIterator[A] {
     private val (first, last) = *.toPair
     private var cur = 0
+
     override def add(e: A) = throw new UnsupportedOperationException
     override def hasNext = cur != last
     override def hasPrevious = cur != first
-    override def next = { val tmp = *(cur); cur += 1; tmp }
+
+    override def next = {
+        if (!hasNext) {
+            throw new NoSuchElementException
+        }
+        val tmp = *(cur)
+        cur += 1
+        tmp
+    }
+
     override def nextIndex = cur
-    override def previous = { cur -= 1; *(cur) }
+
+    override def previous = {
+        if (!hasPrevious) {
+            throw new NoSuchElementException
+        }
+        cur -= 1
+        *(cur)
+    }
+
     override def previousIndex = cur - 1
     override def remove = throw new UnsupportedOperationException
     override def set(e: A) = throw new UnsupportedOperationException
