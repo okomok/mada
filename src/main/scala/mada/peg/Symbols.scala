b890ac1dcefc3aa65d1d6232cf1ee81fea041eb4
unknown <okomok@gmail.com>
2009-01-05 09:49:21 +0900

    trivial tests

--- src/main/scala/mada/peg/Symbols.scala
+++ src/main/scala/mada/peg/Symbols.scala
@@ -10,7 +10,7 @@
 package mada.peg
 
 
-class Symbols[A](is: Iterator[A]*)(implicit c: A => Ordered[A]) extends Parser[A] {
+class Symbols[A](vs: Vector[A]*)(implicit c: A => Ordered[A]) extends Parser[A] {
     override def parse(v: Vector[A], first: Long, last: Long): Long = {
         set.parse(v, first, last)
     }
@@ -28,47 +28,58 @@ class Symbols[A](is: Iterator[A]*)(implicit c: A => Ordered[A]) extends Parser[A
 class TSTreeMap[A, V](lt: (A, A) => Boolean) {
     private var root: Node = null
 
+    override def toString: String = {
+        new StringBuilder().append("<tstreemap>").append(root).append("</tstreemap>").toString
+    }
+
+    def contains(key: Vector[A]): Boolean = {
+        get(key) != None
+    }
+
     def get(key: Vector[A]): Option[V] = {
         val (first, last) = key.toPair
         get(key, first, last)
     }
 
-    def get(key: Vector[A], first: Long, last: Long): Option[V] = {
-        // `first == last` is ok; it just returns `None`.
-
-        val node = Node.findNode(root, key, first, last)
-        if (node == null) {
-            None
-        } else {
-            node.value
-        }
+    def parse(key_ : Vector[A]): Long = {
+        val (first, last) = key_.toPair
+        parse(key_, first, last)
     }
 
-    def put(key: Iterator[A], value: V): V = {
-        Assert(key.hasNext)
+    def put(key: Vector[A], value: V): V = {
+        val (first, last) = key.toPair
+        put(key, first, last, value)
+    }
 
-        if (root == null) {
-            root = new Node(key.next)
-        }
-
-        Node.copyInto(key, root).value = Some(value)
-        value
+    def get(key: Vector[A], first: Long, last: Long): Option[V] = {
+        val (n, _, _) = Node.parse(root, key, first, last)
+        if (n == null) None else n.value
     }
 
     def parse(key_ : Vector[A], first: Long, last: Long): Long = {
-        // `first == last` is ok; it just returns `FAILED`.
+        val (_, n, i) = Node.parse(root, key_, first, last)
+        if (n == null || n.value.isEmpty) Parser.FAILED else i
+    }
 
-        Node.parse(root, key_, first, last)
+    def put(key: Vector[A], first: Long, last: Long, value: V): V = {
+        Assert(first != last)
+
+        if (root == null) {
+            root = new Node(key(first))
+        }
+        Node.copyInto(key, first, last, root).value = Some(value)
+        value
     }
 
     object Node {
-        def copyInto(first: Iterator[A], _result: Node): Node = {
+        def copyInto(key: Vector[A], _first: Long, last: Long, _result: Node): Node = {
+            Assert(_first != last)
             Assert(_result != null)
-
+            var first = _first
             var result = _result
 
-            while (first.hasNext) {
-                val k = first.next
+            var k = key(first)
+            while (true) {
                 if (result > k) {
                     if (result.left == null) {
                         result.left = new Node(k)
@@ -79,9 +90,15 @@ class TSTreeMap[A, V](lt: (A, A) => Boolean) {
                         result.right = new Node(k)
                     }
                     result = result.right
-                } else if (first.hasNext) {
+                } else {
+                    first += 1
+                    if (first == last) {
+                        return result
+                    }
+
+                    k = key(first)
                     if (result.middle == null) {
-                        result.middle = new Node(first.next)
+                        result.middle = new Node(k)
                     }
                     result = result.middle
                 }
@@ -90,49 +107,33 @@ class TSTreeMap[A, V](lt: (A, A) => Boolean) {
             result
         }
 
-        def findNode(_first1: Node, key2: Vector[A], _first2: Long, last2: Long): Node = {
+        def parse(_first1: Node, key2: Vector[A], _first2: Long, last2: Long): (Node, Node, Long) = {
             Assert(_first1 != null)
-
             var first1 = _first1
+            var cur1: Node = null
             var first2 = _first2
 
-            while (first1 != null && first2 != last2) {
-                val k2 = key2(first2)
-                if (first1 > k2) {
-                    first1 = first1.left
-                } else if (first1 < k2) {
-                    first1 = first1.right
-                } else {
-                    first1 = first1.middle; first2 += 1
+            if (first2 != last2) {
+                var k2 = key2(first2)
+                while (first1 != null) {
+                    if (first1 > k2) {
+                        first1 = first1.left
+                    } else if (first1 < k2) {
+                        first1 = first1.right
+                    } else {
+                        cur1 = first1
+                        first2 += 1
+                        if (first2 == last2) {
+                            return (first1, cur1, first2)
+                        }
+
+                        k2 = key2(first2)
+                        first1 = first1.middle
+                    }
                 }
             }
 
-            // If you loved heap, this could return `(first1, first2)`.
-            first1
-        }
-
-        def parse(_first1: Node, key2: Vector[A], _first2: Long, last2: Long): Long = {
-            Assert(_first1 != null)
-
-            var first1 = _first1
-            var first2 = _first2
-
-            while (first1 != null && first2 != last2) {
-                val k2 = key2(first2)
-                if (first1 > k2) {
-                    first1 = first1.left
-                } else if (first1 < k2) {
-                    first1 = first1.right
-                } else {
-                    first1 = first1.middle; first2 += 1
-                }
-            }
-
-            if (first1 == null || !first1.value.isEmpty) {
-                first2
-            } else {
-                Parser.FAILED
-            }
+            (first1, cur1, first2)
         }
     }
 
@@ -144,5 +145,24 @@ class TSTreeMap[A, V](lt: (A, A) => Boolean) {
 
         def <(e: A): Boolean = lt(elem, e)
         def >(e: A): Boolean = lt(e, elem)
+
+        override def toString = {
+            val sb = new StringBuilder()
+            sb.append("<node>")
+                sb.append("<elem>").append(elem).append("</elem>")
+            if (!value.isEmpty) {
+                sb.append("<value>").append(value.get).append("</value>")
+            }
+            if (left != null) {
+                sb.append("<left>").append(left).append("</left>")
+            }
+            if (middle != null) {
+                sb.append("<middle>").append(middle).append("</middle>")
+            }
+            if (right != null) {
+                sb.append("<right>").append(right).append("</right>")
+            }
+            sb.append("</node>").toString
+        }
     }
 }
--- src/test/scala/madatest/peg/SymbolsTest.scala
+++ src/test/scala/madatest/peg/SymbolsTest.scala
@@ -7,25 +7,40 @@
 package madatest.peg
 
 
+// See: http://www.javaworld.com/javaworld/jw-02-2001/jw-0216-ternary.html
+
+
 import mada.peg._
 import junit.framework.Assert._
 import mada.vec.Compatibles._
-import mada.StringCompatibles._
 
 
 class SymbolsTest {
     def testTrivial: Unit = {
-        val map = new TSTreeMap[Char, Int](mada.vec.stl.Less[Char])
-        map.put("pineapple", 10)
-        map.put("orange", 11)
-        map.put("banana", 12)
-        map.put("applepie", 13)
-        map.put("apple", 14)
+        val map = new TSTreeMap[Char, String](mada.vec.stl.Less[Char])
+        map.put("to", "to")
+        println(map.toString)
+        map.put("too", "too")
+        println(map.toString)
+        map.put("tot", "tot")
+        println(map.toString)
+        map.put("tab", "tab")
+        println(map.toString)
+        map.put("so", "so")
+        println(map.toString)
 
-        assertEquals(10, map.get("pineapple").get)
-        assertEquals(11, map.get("orange").get)
-        assertEquals(12, map.get("banana").get)
-        assertEquals(13, map.get("applepie").get)
-        assertEquals(14, map.get("apple").get)
+        assertEquals("so", map.get("so").get)
+        assertEquals("tab", map.get("tab").get)
+        assertEquals("to", map.get("to").get)
+        assertEquals("too", map.get("too").get)
+        assertEquals("tot", map.get("tot").get)
+        assertFalse(map.contains("")) // works.
+
+        assertEquals(Parser.FAILED, map.parse("ztot"))
+        assertEquals(Parser.FAILED, map.parse("t"))
+        assertEquals(Parser.FAILED, map.parse("tzzzzz"))
+        assertEquals(Parser.FAILED, map.parse(""))
+        assertEquals(3L, map.parse("tot"))
+        assertEquals(3L, map.parse("totzzzzz"))
     }
 }
