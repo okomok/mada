c042ce5a17f6ba82f90e4792fe42316d8849b547
unknown <okomok@gmail.com>
2009-01-22 11:34:54 +0900

    optimize

--- src/main/scala/mada/vec/parallel/Count.scala
+++ src/main/scala/mada/vec/parallel/Count.scala
@@ -9,13 +9,13 @@ package mada.vec.parallel
 
 object Count {
     def apply[A](v: Vector[A], p: A => Boolean, grainSize: Int): Int = {
+        val n = new java.util.concurrent.atomic.AtomicInteger(0)
+        v.parallel(grainSize).foreach({ e => if (p(e)) n.incrementAndGet })
+        n.get
+/*
         v.divide(grainSize).
             parallel(1).map({ w => w.count(p) }).
                 reduceLeft(_ + _)
-/* maybe faster
-        val n = new java.util.concurrent.atomic.AtomicInt(0)
-        v.parallel(grainSize).foreach({ e => if (p(e)) n.incrementAndGet })
-        n.get
 */
     }
 }
