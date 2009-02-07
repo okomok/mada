e3949797e0c40e65c7a578b1b52d4d93974304b5
unknown <okomok@gmail.com>
2009-02-08 05:02:41 +0900

    nomsg

--- src/main/scala/mada/peg/FilterFrom.scala
+++ src/main/scala/mada/peg/FilterFrom.scala
@@ -8,5 +8,7 @@ package mada.peg
 
 
 private[mada] object FilterFrom {
-    def apply[A](p: Peg[A], v: Vector[A]): Iterator[A] = Iterator.flatten(p.tokenize(v).map({ v => v.toIterator }))
+    def apply[A](p: Peg[A], v: Vector[A]): Iterator[A] = {
+        Iterator.flatten(p.tokenize(v).map({ v => v.toIterator }))
+    }
 }
