43f4de85b0d32f9d2c4129af698996842fbd7ab4
unknown <okomok@gmail.com>
2009-02-17 13:44:46 +0900

    nomsg

--- src/main/scala/mada/comp/Compatibles.scala
+++ src/main/scala/mada/comp/Compatibles.scala
@@ -15,32 +15,32 @@ object Compatibles {
     import Compare._
 
     /**
-     * Alias of <code>fromOrderedView</code>
+     * Alias of <code>Compare.fromOrderedView</code>
      */
     implicit def madaCompareFromOrderedView[A](from: OrderedView[A]): Type[A] = fromOrderedView(from)
 
     /**
-     * Alias of <code>toOrderedView</code>
+     * Alias of <code>Compare.toOrderedView</code>
      */
     implicit def madaCompareToOrderedView[A](from: Type[A]): OrderedView[A] = toOrderedView(from)
 
     /**
-     * Alias of <code>fromOrdering</code>
+     * Alias of <code>Compare.fromOrdering</code>
      */
     implicit def madaCompareFromOrdering[A](from: Ordering[A]): Type[A] = fromOrdering(from)
 
     /**
-     * Alias of <code>toOrdering</code>
+     * Alias of <code>Compare.toOrdering</code>
      */
     implicit def madaCompareToOrdering[A](from: Type[A]): Ordering[A] = toOrdering(from)
 
     /**
-     * Alias of <code>fromComparator</code>
+     * Alias of <code>Compare.fromComparator</code>
      */
     implicit def madaCompareFromComparator[A](from: Comparator[A]): Type[A] = fromComparator(from)
 
     /**
-     * Alias of <code>toComparator</code>
+     * Alias of <code>Compare.toComparator</code>
      */
     implicit def madaCompareToComparator[A](from: Type[A]): Comparator[A] = toComparator(from)
 }
