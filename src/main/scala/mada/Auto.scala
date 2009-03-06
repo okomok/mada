3be752c72c33b9aeaf883e0ae8d94df28e8bd679
unknown <okomok@gmail.com>
2009-03-06 13:32:39 +0900

    scala maven in central.

--- src/main/scala/mada/Auto.scala
+++ src/main/scala/mada/Auto.scala
@@ -17,7 +17,7 @@ object Auto {
     def apply[A](a: A)(f: A => Unit)(implicit c: A => Auto): Unit = using(a)(f)
 
     /**
-     * @return  <code>try { f(a) } finally { a.dispose }</code>.
+     * @return  <code>try { f(a) } finally { c(a).dispose }</code>.
      */
     def using[A](a: A)(f: A => Unit)(implicit c: A => Auto): Unit = try { f(a) } finally { c(a).dispose }
 }
