f5f03012157bff6d93c7f92a1620700cb06f2c3e
unknown <okomok@gmail.com>
2009-04-01 17:12:25 +0900

    nomsg

--- src/main/scala/mada/vec/para/Sort.scala
+++ src/main/scala/mada/vec/para/Sort.scala
@@ -22,28 +22,28 @@ private[mada] object Sort {
 
     def impl[A](v: Vector[A], first: Int, last: Int, lt: Compare.Func[A], grainSize: Int): Unit = {
         if (first != last) {
-            val us = new ArrayList[() => Unit]
-            loop(v, first, last, depthLimit(first, last), lt, grainSize, us)
-            Vector.from(us).each{ u => u() } // join all.
+            val rs = new ArrayList[Vector[A]]
+            loop(v, first, last, depthLimit(first, last), lt, grainSize, rs)
+            Vector.fromJclList(rs).parallel(1).each{_.sortBy(lt)}
         }
     }
 
     // See: stl.IntroSort
-    def loop[A](* : Vector[A], __first: Int, last: Int, depth_limit: Int, __comp: Compare.Func[A], grainSize: Int, us: ArrayList[() => Unit]): Unit = {
+    def loop[A](* : Vector[A], __first: Int, last: Int, depth_limit: Int, __comp: Compare.Func[A], grainSize: Int, rs: ArrayList[Vector[A]]): Unit = {
         var __last = last
         var __depth_limit = depth_limit
 
         while (__last - __first > grainSize) {
             if (__depth_limit == 0) {
-                us.add(future{*(__first, __last).sortBy(__comp)})
+                rs.add(*(__first, __last))
                 return
             }
             __depth_limit -= 1
             val __cut = UnguardedPartition(*, __first, __last, Median(*(__first), *(__first + (__last - __first)/2), *(__last - 1), __comp), __comp)
-            loop(*, __cut, __last, __depth_limit, __comp, grainSize, us)
+            loop(*, __cut, __last, __depth_limit, __comp, grainSize, rs)
             __last = __cut
         }
 
-        us.add(future{*(__first, __last).sortBy(__comp)})
+        rs.add(*(__first, __last))
     }
 }
