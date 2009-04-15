55eb863a78006b1517499f68dc65def29d3e0396
unknown <okomok@gmail.com>
2009-04-15 19:20:16 +0900

    Adds null tests.

---
+++ src/test/scala/madatest/toy/NullTest.scala
@@ -0,0 +1,33 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest.toy.null_
+
+
+import junit.framework.Assert._
+
+
+// See: http://www.haskell.org/haskellwiki/Existential_types
+class NullTest {
+    def testMatch: Unit = {
+        assertFalse(null == 10)
+        assertFalse(null.isInstanceOf[String])
+        assertFalse(null.asInstanceOf[String].isInstanceOf[String])
+
+        null.asInstanceOf[String] match {
+            case s: String => fail("doh")
+            case null => // println("wow")
+        }
+        // BTW, Any.== passes null to Any.equals.
+        ()
+    }
+
+    def testAsInstanceOf: Unit = {
+        null.asInstanceOf[String] // ok cuz String <: AnyRef.
+        // AssertThrows(null.asInstanceOf[Int]) // should throw due to SLS-6.3?
+        // assertTrue(10, null.asInstanceOf[Int] + 10) // ok but probably unspecified.
+    }
+}
---
+++ src/test/scala/madatest/vec/EqualsTest.scala
@@ -0,0 +1,18 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest.vec
+
+
+import mada.Vector
+import junit.framework.Assert._
+
+
+class EqualsTest {
+    def testNull: Unit = {
+        assertFalse(Vector.range(1, 3) == null)
+    }
+}
