614f08a49609418885eebaec426e53a0825a8713
unknown <okomok@gmail.com>
2009-04-18 22:00:04 +0900

    Prefers Iterable to Seq.

--- src/main/scala/mada/func/Parameterize.scala
+++ src/main/scala/mada/func/Parameterize.scala
@@ -11,23 +11,23 @@ package mada.func
  * Converts a function into parameterized one.
  */
 trait Parameterize { this: Functions.type =>
-    def parameterize1[T1, R](f: Function1[T1, R])(q1: Parameter[T1]): Function1[Seq[Parameter[_]], R] = new Function1[Seq[Parameter[_]], R] {
-        override def apply(ps: Seq[Parameter[_]]) = f(getArg(ps, q1))
+    private def getArg[T](ps: Iterable[Parameter[_]], q: Parameter[T]) = ps.find(_.origin eq q.origin).getOrElse(q).argument.asInstanceOf[T]
+
+    def parameterize1[T1, R](f: Function1[T1, R])(q1: Parameter[T1]): Function1[Iterable[Parameter[_]], R] = new Function1[Iterable[Parameter[_]], R] {
+        override def apply(ps: Iterable[Parameter[_]]) = f(getArg(ps, q1))
     }
 
-    def parameterize2[T1, T2, R](f: Function2[T1, T2, R])(q1: Parameter[T1], q2: Parameter[T2]): Function1[Seq[Parameter[_]], R] = new Function1[Seq[Parameter[_]], R] {
-        override def apply(ps: Seq[Parameter[_]]) = f(getArg(ps, q1), getArg(ps, q2))
+    def parameterize2[T1, T2, R](f: Function2[T1, T2, R])(q1: Parameter[T1], q2: Parameter[T2]): Function1[Iterable[Parameter[_]], R] = new Function1[Iterable[Parameter[_]], R] {
+        override def apply(ps: Iterable[Parameter[_]]) = f(getArg(ps, q1), getArg(ps, q2))
     }
 
-    def parameterize3[T1, T2, T3, R](f: Function3[T1, T2, T3, R])(q1: Parameter[T1], q2: Parameter[T2], q3: Parameter[T3]): Function1[Seq[Parameter[_]], R] = new Function1[Seq[Parameter[_]], R] {
-        override def apply(ps: Seq[Parameter[_]]) = f(getArg(ps, q1), getArg(ps, q2), getArg(ps, q3))
+    def parameterize3[T1, T2, T3, R](f: Function3[T1, T2, T3, R])(q1: Parameter[T1], q2: Parameter[T2], q3: Parameter[T3]): Function1[Iterable[Parameter[_]], R] = new Function1[Iterable[Parameter[_]], R] {
+        override def apply(ps: Iterable[Parameter[_]]) = f(getArg(ps, q1), getArg(ps, q2), getArg(ps, q3))
     }
 
-    def parameterize[T1, R](f: Function1[T1, R])(q1: Parameter[T1]): Function1[Seq[Parameter[_]], R] = parameterize1(f)(q1)
-    def parameterize[T1, T2, R](f: Function2[T1, T2, R])(q1: Parameter[T1], q2: Parameter[T2]): Function1[Seq[Parameter[_]], R] = parameterize2(f)(q1, q2)
-    def parameterize[T1, T2, T3, R](f: Function3[T1, T2, T3, R])(q1: Parameter[T1], q2: Parameter[T2], q3: Parameter[T3]): Function1[Seq[Parameter[_]], R] = parameterize3(f)(q1, q2, q3)
-
-    private def getArg[T](ps: Seq[Parameter[_]], q: Parameter[T]) = ps.find(_.origin eq q.origin).getOrElse(q).argument.asInstanceOf[T]
+    def parameterize[T1, R](f: Function1[T1, R])(q1: Parameter[T1]): Function1[Iterable[Parameter[_]], R] = parameterize1(f)(q1)
+    def parameterize[T1, T2, R](f: Function2[T1, T2, R])(q1: Parameter[T1], q2: Parameter[T2]): Function1[Iterable[Parameter[_]], R] = parameterize2(f)(q1, q2)
+    def parameterize[T1, T2, T3, R](f: Function3[T1, T2, T3, R])(q1: Parameter[T1], q2: Parameter[T2], q3: Parameter[T3]): Function1[Iterable[Parameter[_]], R] = parameterize3(f)(q1, q2, q3)
 }
 
 
