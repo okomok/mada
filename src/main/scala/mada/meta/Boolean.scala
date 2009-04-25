73267a9fda33c8f1cee974fed4f93fa7a28ccce9
unknown <okomok@gmail.com>
2009-04-25 13:33:50 +0900

    Adds lazyIf

--- src/main/scala/mada/meta/Boolean.scala
+++ src/main/scala/mada/meta/Boolean.scala
@@ -20,7 +20,8 @@ trait Booleans { this: Meta.type =>
         type or[that <: Boolean] <: Boolean
         type not <: Boolean
 
-        private[mada] type if_[then <: Object, else_ <: then] <: then
+        private[mada] type if_[then <: Object, _else <: then] <: then
+        private[mada] type lazyIf[then <: Function0, _else <: Function0 { type Result0 <: then#Result0 }] <: then#Result0
     }
 
     final class `true` extends Boolean {
@@ -28,7 +29,8 @@ trait Booleans { this: Meta.type =>
         override type or[that <: Boolean] = `true`
         override type not = `false`
 
-        private[mada] override type if_[then <: Object, else_ <: then] = then
+        private[mada] override type if_[then <: Object, _else <: then] = then
+        private[mada] override type lazyIf[then <: Function0, _else <: Function0 { type Result0 <: then#Result0 }] = then#apply0
     }
 
     final class `false` extends Boolean {
@@ -36,7 +38,8 @@ trait Booleans { this: Meta.type =>
         override type or[that <: Boolean] = that
         override type not = `true`
 
-        private[mada] override type if_[then <: Object, else_ <: then] = else_
+        private[mada] override type if_[then <: Object, _else <: then] = _else
+        private[mada] override type lazyIf[then <: Function0, _else <: Function0 { type Result0 <: then#Result0 }] = _else#apply0
     }
 
     // TODO: Move to Operators.
@@ -44,6 +47,7 @@ trait Booleans { this: Meta.type =>
     type ||[a <: Boolean, b <: Boolean] = a#or[b]
     type ![a <: Boolean] = a#not
 
-    type `if`[cond <: Boolean, then <: Object, else_ <: then] = cond#if_[then, else_]
+    type `if`[cond <: Boolean, then <: Object, _else <: then] = cond#if_[then, _else]
+    type lazyIf[cond <: Boolean, then <: Function0, _else <: Function0 { type Result0 <: then#Result0 }] = cond#lazyIf[then, _else]
 
 }
