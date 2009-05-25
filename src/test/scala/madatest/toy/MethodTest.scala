91d14b14b1bc2d189c7464d3680a13daddba8dab
unknown <okomok@gmail.com>
2009-05-26 02:00:23 +0900

    nomsg

---
+++ src/test/scala/madatest/toy/MethodTest.scala
@@ -0,0 +1,24 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest.toy.methodtest
+
+
+import junit.framework.Assert._
+
+
+object foo {
+    def apply[A](e: A) = e
+}
+
+
+class MethodTest {
+
+    def testTrivial: Unit = {
+        assertEquals(3, (foo(_: Int))(3))
+    }
+
+}
