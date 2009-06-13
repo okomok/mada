69cc4c237086646f6f827afeeeb0f27ce0ae16f9
unknown <okomok@gmail.com>
2009-06-13 12:56:21 +0900

    Adds equals tests.

---
+++ src/test/scala/madatest/sequence/EqualsTest.scala
@@ -0,0 +1,77 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest.sequencetest
+
+
+import mada.sequence._
+import junit.framework.Assert._
+
+
+class EqualsTest {
+    def testTrivial: Unit = {
+        val I1: Iterative[Int] = iterative.Of(27,10,14,7,4,1,2)
+        val I2: Iterative[Int] = iterative.Of(27,10,14,7,4,1,2)
+        val V1: Vector[Int] = vector.Of(27,10,14,7,4,1,2)
+        val V2: Vector[Int] = vector.Of(27,10,14,7,4,1,2)
+        val L1: List[Int] = list.Of(27,10,14,7,4,1,2)
+        val L2: List[Int] = list.Of(27,10,14,7,4,1,2)
+
+        assertEquals(I1, I2)
+        assertEquals(I1, V2)
+        assertEquals(I1, L2)
+
+        assertEquals(V1, I2)
+        assertEquals(V1, V2)
+        assertEquals(V1, L2)
+
+        assertEquals(L1, I2)
+        assertEquals(L1, V2)
+        assertEquals(L1, L2)
+    }
+
+    def testNot: Unit = {
+        val I1: Iterative[Int] = iterative.Of(27,10,14,7,4,1,2)
+        val I2: Iterative[Int] = iterative.Of(27,10,14,7,40,1,2)
+        val V1: Vector[Int] = vector.Of(27,10,14,7,4,1,2)
+        val V2: Vector[Int] = vector.Of(27,10,14,7,40,1,2)
+        val L1: List[Int] = list.Of(27,10,14,7,4,1,2)
+        val L2: List[Int] = list.Of(27,10,14,7,40,1,2)
+
+        AssertNotEquals(I1, I2)
+        AssertNotEquals(I1, V2)
+        AssertNotEquals(I1, L2)
+
+        AssertNotEquals(V1, I2)
+        AssertNotEquals(V1, V2)
+        AssertNotEquals(V1, L2)
+
+        AssertNotEquals(L1, I2)
+        AssertNotEquals(L1, V2)
+        AssertNotEquals(L1, L2)
+    }
+
+    def testEmpty: Unit = {
+        val I1: Iterative[Int] = iterative.emptyOf[Int]
+        val I2: Iterative[Int] = iterative.emptyOf[Int]
+        val V1: Vector[Int] = vector.emptyOf[Int]
+        val V2: Vector[Int] = vector.emptyOf[Int]
+        val L1: List[Int] = list.emptyOf[Int]
+        val L2: List[Int] = list.emptyOf[Int]
+
+        assertEquals(I1, I2)
+        assertEquals(I1, V2)
+        assertEquals(I1, L2)
+
+        assertEquals(V1, I2)
+        assertEquals(V1, V2)
+        assertEquals(V1, L2)
+
+        assertEquals(L1, I2)
+        assertEquals(L1, V2)
+        assertEquals(L1, L2)
+    }
+}
