18023e4d8d4eff4d13b47d5eb28288754c57b321
unknown <okomok@gmail.com>
2009-06-15 19:47:12 +0900

    nomsg

--- src/test/scala/madatest/sequence/vector/DocTest.scala
+++ src/test/scala/madatest/sequence/vector/DocTest.scala
@@ -12,14 +12,14 @@ import junit.framework.Assert._
 
 class DocTest {
     def testTrivial: Unit = {
-        val v = Vector(0,1,2,3,4).toParallel
-        v.map(_ + 10).seek(_ == 13) match {
+        val v = Vector(0,1,2,3,4)
+        v.parallel.map(_ + 10).parallel.seek(_ == 13) match {
             case Some(e) => assertEquals(13, e)
             case None => fail("doh")
         }
 
         val i = new java.util.concurrent.atomic.AtomicInteger(0)
-        v.each {
+        v.parallel.each {
             _ => i.incrementAndGet
         }
         assertEquals(5, i.get)
