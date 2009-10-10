a92503775610d7ecf8228907c8dc1cb4185af351
unknown <okomok@gmail.com>
2009-10-10 17:29:05 +0900

    toArray woes

--- src/test/scala/madatest/sequence/iterative/JavaTest.scala
+++ src/test/scala/madatest/sequence/iterative/JavaTest.scala
@@ -14,8 +14,8 @@ import junit.framework.Assert._
 class JavaTest {
 
     def testJioReader: Unit = {
-//        val arr = mada.sequence.Vector('a','b','c').toArray // hmm, mada.newArray goes wrong?
-        val arr = Array('a','b','c')
+        val arr = mada.sequence.Vector('a','b','c').toArray // hmm, mada.newArray goes wrong?
+//        val arr = Array('a','b','c')
         val tr = iterative.from(new java.io.CharArrayReader(arr))
         assertEquals(iterative.Of('a','b','c'), tr)
         assertEquals(iterative.Of('a','b','c'), tr) // traverse again.
