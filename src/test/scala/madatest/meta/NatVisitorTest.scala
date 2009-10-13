70866446756594fb02db736191a86b7dc00dabc6
unknown <okomok@gmail.com>
2009-10-14 04:08:34 +0900

    implicit and visitor

---
+++ src/test/scala/madatest/meta/NatVisitorTest.scala
@@ -0,0 +1,50 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest; package metatest
+
+
+import mada.meta._
+//import junit.framework.Assert._
+
+
+class NatVisitorTest {
+
+    trait vt[m <: Nat] extends nat.Visitor {
+        type Result = Nat
+        type visitZero = m
+        type visitSucc[n <: Nat] = n#accept[vt[Succ[m]]]
+    }
+
+    type add[n <: Nat, m <: Nat] = n#accept[vt[m]]
+
+    type add1[n <: Nat, m <: Nat] = n#accept[vt[m]]#increment // doesn't work.
+
+    type inc3[n <: Nat] = n#increment#increment#decrement#increment#increment // works.
+
+    type addx[n <: Nat, m <: Nat] = n#add[m] // works.
+    assertSame[addx[_4N, _3N], _7N]
+
+    type addx1[n <: Nat, m <: Nat] = n#add[m]#increment // works!
+    assertSame[addx1[_4N, _3N], _8N]
+
+    type addxx1[n <: Nat, m <: Nat] = add[n, m]#increment // doesn't work.
+    // assertSame[addxx1[_4N, _3N], _8N]
+
+    assertSame[inc3[_2N], _5N]
+
+    assertSame[add[_3N, _0N], _3N]
+    assertSame[add[_3N, _2N], _5N]
+
+    def testTrivial: Unit = {
+        // println(unmeta[add1[_3N, _2N], scala.Int]) // could not find implicit value
+        // println(unmeta[add[_3N, _2N]#increment, scala.Int])
+    }
+
+    assertSame[add[_3N, _2N]#increment, _6N] // ok
+//    assertSame[add1[_3N, _2N], _6N] // error
+
+}
---
+++ src/test/scala/madatest/toy/Animal2Test.scala
@@ -0,0 +1,60 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest; package toy; package animal2test
+
+
+//import junit.framework.Assert._
+
+class Food
+abstract class Animal {
+    type Self <: Animal
+    type SuitableFood <: Food
+    def eat(f: SuitableFood): Unit = ()
+    def iam(s: Self): Unit = ()
+}
+
+class Grass extends Food
+class Cow extends Animal {
+    override type Self = Cow
+    override type SuitableFood = Grass
+}
+
+class Fish extends Food
+
+
+class Animal2Test {
+
+    def foo(bessy: Cow): Unit = {
+//        bessy.eat(12) // required bessy.SuitableFood
+        bessy.eat(new bessy.SuitableFood)
+        bessy.eat(new Grass)
+        bessy.eat(new Cow#SuitableFood)
+        ()
+    }
+
+    def buz(bessy: Cow, bessy2: Cow): Unit = {
+        mada.meta.assertSame[bessy.Self, Cow]
+//        bessy.iam(12) // required bessy.Self
+        bessy.iam(bessy2)
+        bessy.iam(bessy)
+        ()
+    }
+
+    def bar(bessy: Animal): Unit = {
+//        bessy.iam(12) // required bessy.Self
+//        bessy.iam(bessy) // required bessy.Self
+        ()
+    }
+
+    /* illegal dependent method type (without -Xexperimental)
+    def fuz(bessy: Animal, self: bessy.Self): Unit = {
+        bessy.iam(self) // required bessy.Self
+        ()
+    }
+    */
+}
+
