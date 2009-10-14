4adc52ae4bb9555c1582040484398edf56125871
unknown <okomok@gmail.com>
2009-10-14 14:32:36 +0900

    Visitor woes

---
+++ src/test/scala/madatest/toy/TypeIdentity2Test.scala
@@ -0,0 +1,96 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest; package metatest; package toy; package typeidentity2test
+
+
+import mada.meta.assertSame
+//import junit.framework.Assert._
+
+
+sealed trait Nat {
+    type `this` <: Nat
+
+    type increment <: Nat
+    type add[that <: Nat] <: Nat
+
+    type accept[v <: Visitor] <: v#Result
+}
+
+trait Visitor {
+    type Result // ABSTRACT RESULT TYPE does the bad thing.
+    type visitZero <: Result
+    type visitSucc[n <: Nat] <: Result
+}
+
+sealed trait Zero extends Nat {
+    override type `this` = Zero
+    override type increment = Succ[Zero]
+    override type add[that <: Nat] = that
+    override type accept[v <: Visitor] = v#visitZero
+}
+
+sealed trait Succ[n <: Nat] extends Nat {
+    override type `this` = Succ[n]
+    override type increment = Succ[`this`]
+    override type add[that <: Nat] = Succ[n#add[that]]
+    override type accept[v <: Visitor] = v#visitSucc[n]
+}
+
+
+class TypeIdentity2Test {
+
+    type _0N = Zero
+    type _1N = Succ[_0N]
+    type _2N = Succ[_1N]
+    type _3N = Succ[_2N]
+    type _4N = Succ[_3N]
+    type _5N = Succ[_4N]
+    type _6N = Succ[_5N]
+    type _7N = Succ[_6N]
+    type _8N = Succ[_7N]
+    type _9N = Succ[_8N]
+    type _10N = Succ[_9N]
+
+
+    trait vt[m <: Nat] extends Visitor {
+        type Result = Nat
+        type visitZero = m
+        type visitSucc[n <: Nat] = n#accept[vt[Succ[m]]]
+    }
+
+    trait test1 {
+        type goo[n <: Nat] = _2N#accept[vt[n]]#increment
+        type goh[n <: Nat] = _2N#accept[vt[n]]
+        assertSame[goh[_3N]#increment, _6N]
+        assertSame[goo[_3N], _6N] // ok!!!!!!!!!
+    }
+
+    trait test2 {
+        type foo[n <: Nat] = n#accept[vt[_2N]]#increment
+        type foh[n <: Nat] = n#accept[vt[_2N]]
+        assertSame[foh[_3N]#increment, _6N]
+       // assertSame[foo[_3N], _6N] // error
+    }
+
+    trait test21 {
+        // n#accept[vt[_2N]]
+        // -> vt[_2N]#visitSucc[n-1]
+        // -> n-1#accept[vt[_2N+1]]
+        // ...
+    }
+
+    type add[n <: Nat, m <: Nat] = n#accept[vt[m]]
+
+    type add1[n <: Nat, m <: Nat] = n#accept[vt[m]]#increment // doesn't work.
+    //assertSame[add1[_3N, _2N], _6N] // error
+
+    type add2[n <: Nat, m <: Nat, k <: Nat] = n#accept[vt[m]]#add[k] // doesn't work.
+    assertSame[add[_3N, _2N]#add[_4N], _9N]
+//    assertSame[add2[_3N, _2N, _4N], _9N] // error
+
+
+}
---
+++ src/test/scala/madatest/toy/TypeIdentityTest.scala
@@ -0,0 +1,77 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest; package toy; package typeidentitytest
+
+
+//import junit.framework.Assert._
+
+import mada.meta.assertSame
+import mada.meta
+
+
+
+
+// nullary
+
+sealed trait Func {
+    type result
+}
+
+sealed trait FuncInt extends Func {
+    override type result = Int
+}
+
+sealed trait id[f <: Func] {
+    type what = Func#result
+}
+
+
+// unary
+
+sealed trait Func1 {
+    type apply[x]
+}
+
+sealed trait Fwd1[f <: Func1] extends Func1 {
+    override type apply[x] = f#apply[x]
+}
+
+sealed trait Func1Id extends Func1 {
+    override type apply[x] = x
+}
+
+
+
+
+class TypeIdentityTest {
+
+
+
+    assertSame[Func1Id#apply[Double], Double]
+    assertSame[Fwd1[Func1Id]#apply[Double], Double] // ok
+
+    type callFwd1[f <: Func1, x] = Fwd1[f]#apply[x]
+    assertSame[callFwd1[Func1Id, Double], Double] // ok
+
+    type callFwd111[f <: Func1, x] = Fwd1[Fwd1[Fwd1[f]]]#apply[x]
+    assertSame[callFwd111[Func1Id, Double], Double] // ok
+
+
+    val i: FuncInt#result = 3 // ok
+    assertSame[FuncInt#result, Int] // ok
+
+    //val j: id[FuncInt]#what = 4 // error
+    //assertSame[id[FuncInt]#what, Int] // error!!!!!! probably because no "dependent" types.
+
+    type callid[f <: Func] = id[f]#what
+
+    //assertSame[callid[FuncInt], Int] // error
+
+    assertSame[meta.Succ[mada.meta._1N]#equals[meta._2N]#not, meta.`false`]
+
+}
+
