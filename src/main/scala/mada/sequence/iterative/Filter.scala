1d95e9eb449bbe0cfdb0dcb75859cca7b9302d2d
unknown <okomok@gmail.com>
2009-10-28 16:41:19 +0900

    nomsg

--- src/main/scala/mada/sequence/iterative/Filter.scala
+++ src/main/scala/mada/sequence/iterative/Filter.scala
@@ -28,7 +28,7 @@ case class Filter[A](_1: Iterative[A], _2: A => Boolean) extends Iterative[A] {
 
     override def filter(p: A => Boolean) = _1.filter{ e => _2(e) && p(e) } // filter-filter fusion
 
-    override def foreach(f: A => Unit): Unit = { // foreach-filter fusion
+    override def foreach(f: A => Unit) = { // foreach-filter fusion
         val it = _1.begin
         while (it) {
             val e = ~it
