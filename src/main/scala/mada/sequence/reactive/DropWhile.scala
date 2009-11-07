173e28692ab20d0c81b66f30b8fe9c2379fd8eff
unknown <okomok@gmail.com>
2009-11-07 13:55:25 +0900

    reactor seems wrong.

---
+++ src/main/scala/mada/sequence/reactive/DropWhile.scala
@@ -0,0 +1,27 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package mada; package sequence; package reactive
+
+
+case class DropWhile[A](_1: Reactive[A], _2: A => Boolean) extends Reactive[A] {
+    override def subscribe(k: Reactor[A]) = {
+        val j = new Reactor[A] {
+            private var begins = false
+            override def onEnd = k.onEnd
+            override def react(e: A) = {
+                if (!begins) {
+                    if (_2(e)) {
+                        begins = true
+                        k.react(e)
+                    }
+                } else {
+                    k.react(e)
+                }
+            }
+        }
+    }
+}
---
+++ src/main/scala/mada/sequence/reactive/TakeWhile.scala
@@ -0,0 +1,28 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package mada; package sequence; package reactive
+
+
+case class TakeWhile[A](_1: Reactive[A], _2: A => Boolean) extends Reactive[A] {
+    override def subscribe(k: Reactor[A]) = {
+        val j = new Reactor[A] {
+            private var ends = false
+            override def onEnd = ()
+            override def react(e: A) = {
+                if (!ends) {
+                    if (_2(e)) {
+                        k.react(e)
+                    } else {
+                        ends = true
+                        k.onEnd
+                    }
+                }
+            }
+        }
+        _1.subscribe(j)
+    }
+}
