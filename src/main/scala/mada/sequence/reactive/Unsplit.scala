a486a0e900e03794c32c2dd23eb0a52b624cc149
unknown <okomok@gmail.com>
2009-11-18 06:10:24 +0900

    unsplit tests.

--- src/main/scala/mada/sequence/reactive/Unsplit.scala
+++ src/main/scala/mada/sequence/reactive/Unsplit.scala
@@ -10,8 +10,9 @@ package mada; package sequence; package reactive
 case class Unsplit[A](_1: Reactive[Reactive[A]], _2: Reactive[A]) extends Reactive[A] {
     override def subscribe(k: Reactor[A]) = {
         val j = new Reactor[Reactive[A]] {
+            private val pre = new IfFirst[Unit](_ => (), _ => _2.subscribe(k.noEnd))
             override def onEnd = k.onEnd
-            override def react(e: Reactive[A]) = { _2.subscribe(k.noEnd); e.subscribe(k.noEnd) }
+            override def react(e: Reactive[A]) = { pre(); e.subscribe(k.noEnd) }
         }
         _1.subscribe(j)
     }
