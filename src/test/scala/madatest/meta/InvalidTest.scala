1bbf94475943a1915ddfe5f4c54d3a209a829813
unknown <okomok@gmail.com>
2009-12-19 06:38:26 +0900

    generic metatrait mistery

--- src/test/scala/madatest/meta/InvalidTest.scala
+++ src/test/scala/madatest/meta/InvalidTest.scala
@@ -11,6 +11,8 @@ import mada.meta._
 // import junit.framework.Assert._
 
 
+// Generic-metamethod can't be overridden.
+
 class GenericMethodOverrideTest {
 
     trait B {
@@ -32,6 +34,9 @@ class GenericMethodOverrideTest {
 }
 
 
+
+// In summary, metatrait can't be parametarized.
+
 class TypeConstraintTest {
 
     trait B {
@@ -51,5 +56,60 @@ class TypeConstraintTest {
     // NO
     type foo2[b <: B { type R = Nat }, n <: Nat] = b#inc[n]#increment
     // assertSame[foo2[d, _2N], _4N]
+
+}
+
+class TypeConstraint2Test {
+
+    trait B[R] {
+        type inc[n <: Nat] <: R
+    }
+
+    trait d extends B[Nat] {
+        override type inc[n <: Nat] = n#increment
+    }
+
+    // OK
+    type foo1[b <: B[Nat], n <: Nat] = b#inc[n]
+    assertSame[foo1[d, _2N]#increment, _4N]
+
+    // NO
+    type foo2[b <: B[Nat], n <: Nat] = b#inc[n]#increment
+    // assertSame[foo2[d, _2N], _4N]
+
+}
+
+class TypeConstraint3Test {
+
+    trait B {
+        type inc[n <: Nat] <: Nat
+    }
+
+    trait d extends B {
+        override type inc[n <: Nat] = n#increment
+    }
+
+    // OK
+    type foo1[b <: B, n <: Nat] = b#inc[n]
+    assertSame[foo1[d, _2N]#increment, _4N]
+
+    // OK
+    type foo2[b <: B, n <: Nat] = b#inc[n]#increment
+    assertSame[foo2[d, _2N], _4N]
+
+}
+
+
+class TypeConstraint4Test {
+
+    type inc[n <: Nat] = n#increment
+
+    // OK
+    type foo1[_inc[_ <: Nat] <: Nat, n <: Nat] = _inc[n]
+    assertSame[foo1[inc, _2N]#increment, _4N]
+
+    // OK
+    type foo2[_inc[_ <: Nat] <: Nat, n <: Nat] = _inc[n]#increment
+    assertSame[foo2[inc, _2N], _4N]
 
 }
