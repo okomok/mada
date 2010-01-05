849f7d133075119b839950a7daf168e87daa540e
unknown <okomok@gmail.com>
2010-01-06 05:17:52 +0900

    Adds constraint test.

---
+++ src/test/scala/madatest/blend/list/ConstraintTest.scala
@@ -0,0 +1,24 @@
+
+
+// Copyright Shunsuke Sogame 2008-2009.
+// Distributed under the terms of an MIT-style license.
+
+
+package madatest; package blendtest; package listtest
+
+
+import mada.blend._
+import mada.meta
+
+
+class ConstraintTest {
+    import junit.framework.Assert._
+    assertFalse(scala.Nil eq Nil)
+
+    def inc[l <: List { type head <: Int }](_l: l): Int = _l.head + 2
+
+    def testTrivial: Unit = {
+        val lst1 = 1 :: "hello" :: 'a' :: 12 :: Nil
+        assertEquals(3, inc(lst1))
+    }
+}
