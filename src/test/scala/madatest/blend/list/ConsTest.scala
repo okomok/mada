f9a28cd6e2cc1a6abfe9a2802263b0a5621d512b
unknown <okomok@gmail.com>
2010-01-09 02:30:01 +0900

    dependent type mistery

--- src/test/scala/madatest/blend/list/ConsTest.scala
+++ src/test/scala/madatest/blend/list/ConsTest.scala
@@ -12,8 +12,22 @@ import mada.blend._
 
 class ConsTest {
 
-    case class ComposeL[T1, R1, T2 <: List, R2 <: List](f: T1 => R1, g: T2 => R2) extends (T1 :: T2 => R1 :: R2) {
+    /*
+    case class Hmm_ComposeL[T1, R1, T2 <: List, R2 <: List](f: T1 => R1, g: T2 => R2) extends (T1 :: T2 => R1 :: R2) {
         override def apply(xs: T1 :: T2): R1 :: R2 = f(xs.head) :: g(xs.tail)
+    }
+    */
+
+    trait Func1 {
+        type Arg1
+        type Result
+        def apply(v1: Arg1): Result
+    }
+
+    case class ComposeL[f <: Func1, g <: Func1 { type Result <: List }](f: f, g: g) extends Func1 {
+        override type Arg1 = f#Arg1 :: g#Arg1
+        override type Result = f#Result :: g#Result
+        override def apply(xs: Arg1) = f(xs.head) :: g(xs.tail)
     }
 
 }
