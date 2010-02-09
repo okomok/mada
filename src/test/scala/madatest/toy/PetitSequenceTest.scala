4e14f8c5bd358d7b2b08bfd4253e1dd680da739d
unknown <okomok@gmail.com>
2010-02-10 04:58:53 +0900

    Override woes.

--- src/test/scala/madatest/toy/PetitSequenceTest.scala
+++ src/test/scala/madatest/toy/PetitSequenceTest.scala
@@ -16,15 +16,15 @@ import scala.annotation.unchecked.uncheckedVariance
 trait Iter[+A] {
     type Self <: Iter[A]
     val self = this.asInstanceOf[Self]
-    final def map[B, Result](f: A => B)(implicit _map: OverrideMap[Self, A @uncheckedVariance, B, Result]): Result = _map(self, f)
+    def map[B, Result](f: A => B)(implicit _map: SpecializeMap[Self, A @uncheckedVariance, B, Result]): Result = _map(self, f)
 }
 
-trait OverrideMap[-Self <: Iter[A], +A, -B, +Result] extends Function2[Self, A => B, Result] {
+trait SpecializeMap[-Self <: Iter[A], +A, -B, +Result] extends Function2[Self, A => B, Result] {
     def apply(self: Self, f: A => B): Result
 }
 
 object Iter {
-    implicit def overrideMap[A, B] = new OverrideMap[Iter[A], A, B, Iter[B]] {
+    implicit def specializeMap[A, B] = new SpecializeMap[Iter[A], A, B, Iter[B]] {
         override def apply(self: Iter[A], f: A => B): Iter[B] = throw new Error()
     }
 }
@@ -34,7 +34,7 @@ trait Vec[A] extends Iter[A] {
 }
 
 object Vec {
-    implicit def overrideMap[A, B] = new OverrideMap[Vec[A], A, B, Vec[B]] {
+    implicit def specializeMap[A, B] = new SpecializeMap[Vec[A], A, B, Vec[B]] {
         override def apply(self: Vec[A], f: A => B): Vec[B] = throw new Error()
     }
 }
@@ -42,9 +42,9 @@ object Vec {
 
 class PetitSequenceTest {
     def compileMe(i: Int) = {
-//        import Iter.overrideMap
+//        import Iter.specializeMap
         val it = new Iter[Int] { }
-        val jt: Iter[String] = it.map(_.toString) //(overrideMap[Iter[Int], Int, String])
+        val jt: Iter[String] = it.map(_.toString) //(specializeMap[Iter[Int], Int, String])
     }
 
     def compileMe2(i: Int) = {
