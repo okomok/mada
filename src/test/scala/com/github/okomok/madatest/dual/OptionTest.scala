d4c48d80ed784fab5bad98583e9da989c892412b
unknown <okomok@gmail.com>
2010-07-03 21:12:08 +0900

    Adds dual.Option.

---
+++ src/test/scala/com/github/okomok/madatest/dual/OptionTest.scala
@@ -0,0 +1,61 @@
+
+
+// Copyright Shunsuke Sogame 2008-2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.madatest; package dualtest
+
+
+import com.github.okomok.mada.dual
+import dual._
+import junit.framework.Assert._
+
+
+class OptionTest extends junit.framework.TestCase {
+    def testTrivial {
+        type s = Some[Int]
+        val s: s = Some(3)
+        val e: s#get = s.get
+        val k: Int = e
+        assertEquals(3, k)
+
+        type n = None
+        val n: n = None
+        try {
+            val e: n#get = n.get
+            fail("never come here")
+        } catch {
+            case _: NoSuchElementException =>
+        }
+    }
+
+    def testMatch {
+        val s = Some(3)
+        s match {
+            case Some(e) => assertEquals(3, e)
+            case _ => fail("doh")
+        }
+
+        val n: Option = None
+        n match {
+            case Some(e) => fail("doh")
+            case None =>
+        }
+        ()
+    }
+
+    class natty {
+         def apply[o <: Option{ type get <: Nat }](o: o): apply[o] = o.get.increment
+        type apply[o <: Option{ type get <: Nat }] = o#get#increment
+    }
+    val natty = new natty
+
+    def testNatty {
+        type s = Some[_3N]
+        val s = Some(_3N)
+        meta.assertSame[_4N, natty#apply[s]]
+        assert(_4N === natty(s))
+    }
+
+}
--- src/test/scala/com/github/okomok/madatest/dual/list/SliceTest.scala
+++ src/test/scala/com/github/okomok/madatest/dual/list/SliceTest.scala
@@ -4,7 +4,7 @@
 // Distributed under the terms of an MIT-style license.
 
 
-package com.github.okomok.madatest; package dualtest
+package com.github.okomok.madatest; package dualtest; package listtest
 
 
 import com.github.okomok.mada
