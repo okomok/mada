f8bf7f2e7ede850fcc940a0fe9904cdf1fee2dec
unknown <okomok@gmail.com>
2010-07-10 08:38:02 +0900

    Introduces stack.Common.

--- src/main/scala/com/github/okomok/mada/stack/package.scala
+++ src/main/scala/com/github/okomok/mada/stack/package.scala
@@ -7,21 +7,4 @@
 package com.github.okomok.mada
 
 
-package object stack {
-
-    @aliasOf("Stack")
-    type Type[A] = Stack[A]
-
-    @compatibleConversion
-    def from[A](to: Stack[A]): Stack[A] = to
-
-    @compatibleConversion
-    def fromJDeque[A](from: java.util.Deque[A]): Stack[A] = FromJDeque(from)
-
-    @compatibleConversion
-    def fromSStack[A](from: scala.collection.mutable.Stack[A]): Stack[A] = FromSStack(from)
-
-    @compatibleConversion
-    def fromSArrayStack[A](from: scala.collection.mutable.ArrayStack[A]): Stack[A] = FromSArrayStack(from)
-
-}
+package object stack extends stack.Common
