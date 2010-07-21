3a413e5606131d60803c62743e4b32b9ec38b845
unknown <okomok@gmail.com>
2010-07-21 11:27:04 +0900

    hmm

--- project/build/Mada.scala
+++ project/build/Mada.scala
@@ -5,6 +5,8 @@ import sbt._
 
 
 class Mada(info: ProjectInfo) extends DefaultProject(info) {
-    val junit = "junit" % "junit" % "3.8.2" % "test"
+    //val junit = "junit" % "junit" % "3.8.2" % "test"
     override def compileOptions = super.compileOptions ++ compileOptions("-Yrecursion", "50")
+    val junitInterface = "com.novocode" % "junit-interface" % "0.3" % "test"
+    override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
 }
