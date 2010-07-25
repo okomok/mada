4e63d4555d09723bbf1b3602c3413a16b15248ea
unknown <okomok@gmail.com>
2010-07-25 15:18:23 +0900

    hmm

---
+++ src/test/scala/toy/MetamethodReoverrideTest.scala
@@ -0,0 +1,28 @@
+
+
+// Copyright Shunsuke Sogame 2008-2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.madatest; package toy; package metamethodreoverridetest
+
+
+
+import com.github.okomok.mada.dual._
+
+trait A {
+    type isB <: Boolean
+}
+
+trait B extends A {
+    override type isB = `true`
+}
+
+trait C extends A {
+}
+
+object Woo {
+//    val k: C#isB = throw new Error
+//    val i: Int = k
+//    meta.assertSame[Nothing, C#isB]
+}
