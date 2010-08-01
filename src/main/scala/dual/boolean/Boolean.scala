1761f69e5a042e3d3889260fbd58357a431dadd2
unknown <okomok@gmail.com>
2010-08-01 09:23:31 +0900

    AbstractBoolean

--- src/main/scala/dual/boolean/Boolean.scala
+++ src/main/scala/dual/boolean/Boolean.scala
@@ -12,13 +12,11 @@ object Boolean extends Common
 
 
 /**
- * The dual Boolean
+ * The Boolean interface
  */
 sealed trait Boolean extends Any {
     type self <: Boolean
     type asInstanceOfBoolean = self
-//    final override protected  def typeid = _Boolean.typeid
-//    final override protected type typeid = _Boolean.typeid
 
      def not: not
     type not <: Boolean
@@ -26,8 +24,8 @@ sealed trait Boolean extends Any {
      def ===[that <: Boolean](that: that): ===[that]
     type ===[that <: Boolean] <: Boolean
 
-    final  def !==[that <: Boolean](that: that): !==[that] = ===(that).not
-    final type !==[that <: Boolean] = ===[that]#not
+     def !==[that <: Boolean](that: that): !==[that]
+    type !==[that <: Boolean] <: Boolean
 
      def &&[that <: Boolean](that: that): &&[that]
     type &&[that <: Boolean] <: Boolean
@@ -35,14 +33,14 @@ sealed trait Boolean extends Any {
      def ||[that <: Boolean](that: that): ||[that]
     type ||[that <: Boolean] <: Boolean
 
+     def `if`[then <: Function0, _else <: Function0](then: then, _else: _else): `if`[then, _else]
+    type `if`[then <: Function0, _else <: Function0] <: Function0
+
     private[mada]  def isTrue: isTrue
     private[mada] type isTrue <: Boolean
 
     private[mada]  def isFalse: isFalse
     private[mada] type isFalse <: Boolean
-
-     def `if`[then <: Function0, _else <: Function0](then: then, _else: _else): `if`[then, _else]
-    type `if`[then <: Function0, _else <: Function0] <: Function0
 
     final override type undual = scala.Boolean
     final override def canEqual(that: scala.Any) = that.isInstanceOf[Boolean]
@@ -50,64 +48,76 @@ sealed trait Boolean extends Any {
 
 
 /**
- * The dual true
+ * A skeletal implementation of the Boolean interface
  */
-sealed trait `true` extends Boolean {
+sealed trait AbstractBoolean extends Boolean {
+    final override  def !==[that <: Boolean](that: that): !==[that] = ===(that).not
+    final override type !==[that <: Boolean] =                        ===[that]#not
+
+//    final override protected  def typeid = _Boolean.typeid
+//    final override protected type typeid = _Boolean.typeid
+}
+
+
+/**
+ * The true
+ */
+sealed trait `true` extends AbstractBoolean {
     type self = `true`
 
     override  def not: not = `false`
-    override type not = `false`
+    override type not      = `false`
 
     override  def ===[that <: Boolean](that: that): ===[that] = that.isTrue
-    override type ===[that <: Boolean] = that#isTrue
+    override type ===[that <: Boolean]                        = that#isTrue
 
     override  def &&[that <: Boolean](that: that): &&[that] = that
     override type &&[that <: Boolean] = that
 
     override  def ||[that <: Boolean](that: that): ||[that] = `true`
-    override type ||[that <: Boolean] = `true`
-
-    override private[mada]  def isTrue: isTrue = `true`
-    override private[mada] type isTrue = `true`
-
-    override private[mada]  def isFalse: isFalse = `false`
-    override private[mada] type isFalse = `false`
+    override type ||[that <: Boolean]                       = `true`
 
     override  def `if`[then <: Function0, _else <: Function0](then: then, _else: _else): `if`[then, _else] = then
-    override type `if`[then <: Function0, _else <: Function0] = then
+    override type `if`[then <: Function0, _else <: Function0]                                              = then
 
     override def undual: undual = true
+
+    override private[mada]  def isTrue: isTrue = `true`
+    override private[mada] type isTrue         = `true`
+
+    override private[mada]  def isFalse: isFalse = `false`
+    override private[mada] type isFalse          = `false`
 }
 
 
 /**
- * The dual false
+ * The false
  */
-sealed trait `false` extends Boolean {
+sealed trait `false` extends AbstractBoolean {
     type self = `false`
 
     override  def not: not = `true`
-    override type not = `true`
+    override type not      = `true`
 
     override  def ===[that <: Boolean](that: that): ===[that] = that.isFalse
-    override type ===[that <: Boolean] = that#isFalse
+    override type ===[that <: Boolean]                        = that#isFalse
 
     override  def &&[that <: Boolean](that: that): &&[that] = `false`
-    override type &&[that <: Boolean] = `false`
+    override type &&[that <: Boolean]                       = `false`
 
     override  def ||[that <: Boolean](that: that): ||[that] = that
-    override type ||[that <: Boolean] = that
-
-    override private[mada]  def isTrue: isTrue = `false`
-    override private[mada] type isTrue = `false`
-
-    override private[mada]  def isFalse: isFalse = `true`
-    override private[mada] type isFalse = `true`
+    override type ||[that <: Boolean]                       = that
 
     override  def `if`[then <: Function0, _else <: Function0](then: then, _else: _else): `if`[then, _else] = _else
-    override type `if`[then <: Function0, _else <: Function0] = _else
+    override type `if`[then <: Function0, _else <: Function0]                                              = _else
 
     override def undual: undual = false
+
+    override private[mada]  def isTrue: isTrue = `false`
+    override private[mada] type isTrue         = `false`
+
+    override private[mada]  def isFalse: isFalse = `true`
+    override private[mada] type isFalse          = `true`
 }
 
 
