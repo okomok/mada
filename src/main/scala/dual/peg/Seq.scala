063c5ca6d4682d1fd40d8e1e7260e82f6eccdef5
unknown <okomok@gmail.com>
2010-08-13 13:48:22 +0900

    dual.peg.seq

--- src/main/scala/dual/peg/Seq.scala
+++ src/main/scala/dual/peg/Seq.scala
@@ -16,22 +16,34 @@ object Seq {
     final case class Impl[p <: Peg, q <: Peg](p: p, q: q) extends AbstractPeg {
         type self = Impl[p, q]
 
-        override  def parse[xs <: List](xs: xs): parse[xs] = _aux(p.parse(xs))
-        override type parse[xs <: List]                    = _aux[p#parse[xs]]
+        override  def parse[xs <: List](xs: xs): parse[xs] = _aux(p.parse(xs), xs)
+        override type parse[xs <: List]                    = _aux[p#parse[xs], xs]
 
         override  def width: width = p.width.plus(q.width)
         override type width        = p#width#plus[q#width]
 
-        private  def _aux[r <: Result](r: r): _aux[r] =
-            `if`(r.successful, Then(q, r), const0(r)).apply.asInstanceOfPegResult
-        private type _aux[r <: Result] =
-            `if`[r#successful, Then[q, r], const0[r]]#apply#asInstanceOfPegResult
+        private  def _aux[r <: Result, xs <: List](r: r, xs: xs): _aux[r, xs] =
+            `if`(r.successful, Then(q, r, xs), const0(r)).apply.asInstanceOfPegResult
+        private type _aux[r <: Result, xs <: List] =
+            `if`[r#successful, Then[q, r, xs], const0[r]]#apply#asInstanceOfPegResult
     }
 
-    final case class Then[q <: Peg, r <: Result](q: q, r: r) extends Function0 {
-        type self = Then[q, r]
-        override  def apply: apply = q.parse(r.next).map(MakePair(r.get)).asInstanceOf[apply]
-        override type apply        = q#parse[r#next]#map[MakePair[r#get]]
+    final case class Then[q <: Peg, r <: Result, xs <: List](q: q, r: r, xs: xs) extends Function0 {
+        type self = Then[q, r, xs]
+
+        private lazy val s: s = q.parse(r.next)
+        private type s        = q#parse[r#next]
+
+        override  def apply: apply =
+            `if`(s.successful, ThenThen(r, s), const0(Failure(xs))).apply.asInstanceOfPegResult
+        override type apply =
+            `if`[s#successful, ThenThen[r, s], const0[Failure[xs]]]#apply#asInstanceOfPegResult
+    }
+
+    final case class ThenThen[r <: Result, s <: Result](r: r, s: s) extends Function0 {
+        type self = ThenThen[r, s]
+        override  def apply: apply = Success(Pair(r.get, s.get), s.next)
+        override type apply        = Success[Pair[r#get, s#get], s#next]
     }
 
     final case class MakePair[a <: Any](a: a) extends Function1 {
---
+++ src/test/scala/dual/peg/SeqTest.scala
@@ -0,0 +1,77 @@
+
+
+// Copyright Shunsuke Sogame 2008-2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.madatest
+package dualtest; package pegtest
+
+
+import com.github.okomok.mada
+
+import mada.dual._
+import nat.dense.Literal._
+import junit.framework.Assert._
+import peg.fromList
+
+
+class SeqTest extends org.scalatest.junit.JUnit3Suite {
+
+    def testNilInput {
+        type xs    = Nil
+        val xs: xs = Nil
+        type p   = fromList[_3 :: _5 :: _9 :: Nil]#seq[fromList[_4 :: _2 :: Nil]]
+        val p: p = fromList(_3 :: _5 :: _9 :: Nil).seq(fromList(_4 :: _2 :: Nil))
+        type r = p#parse[xs]
+        val r: r = p.parse(xs)
+        meta.assertNot[r#successful]
+        assertFalse(r.successful.undual)
+        meta.assertSame[xs, r#next#force]
+        assertEquals(xs, r.next)
+    }
+
+    def testFail {
+        type xs    = _3 :: _5 :: _9 :: _4 :: _1 :: Nil
+        val xs: xs = _3 :: _5 :: _9 :: _4 :: _1 :: Nil
+        type p   = fromList[_3 :: _5 :: _9 :: Nil]#seq[fromList[_4 :: _2 :: Nil]]
+        val p: p = fromList(_3 :: _5 :: _9 :: Nil).seq(fromList(_4 :: _2 :: Nil))
+        type r = p#parse[xs]
+        val r: r = p.parse(xs)
+        meta.assertNot[r#successful]
+        assertFalse(r.successful.undual)
+        meta.assertSame[xs, r#next]
+        assertEquals(xs, r.next)
+    }
+
+    def testFullMatch {
+        type xs    = _3 :: _5 :: _9 :: _4 :: _2 :: Nil
+        val xs: xs = _3 :: _5 :: _9 :: _4 :: _2 :: Nil
+        type p   = fromList[_3 :: _5 :: _9 :: Nil]#seq[fromList[_4 :: _2 :: Nil]]
+        val p: p = fromList(_3 :: _5 :: _9 :: Nil).seq(fromList(_4 :: _2 :: Nil))
+        type r = p#matches[xs]
+        val r: r = p.matches(xs)
+        meta.assert[r]
+        assertTrue(r.undual)
+    }
+
+    def testSubmatch {
+        type xs    = _3 :: _5 :: _9 :: _4 :: _2 :: _1 :: _5 :: Nil
+        val xs: xs = _3 :: _5 :: _9 :: _4 :: _2 :: _1 :: _5 :: Nil
+        type p   = fromList[_3 :: _5 :: _9 :: Nil]#seq[fromList[_4 :: _2 :: Nil]]
+        val p: p = fromList(_3 :: _5 :: _9 :: Nil).seq(fromList(_4 :: _2 :: Nil))
+        type r = p#parse[xs]
+        val r: r = p.parse(xs)
+        meta.assert[r#successful]
+        assertTrue(r.successful.undual)
+        type e = r#get#asInstanceOfProduct2
+        val e: e = r.get.asInstanceOfProduct2
+        meta.assertSame[_3 :: _5 :: _9 :: Nil, e#_1]
+        assertEquals(_3 :: _5 :: _9 :: Nil, e._1)
+        meta.assertSame[_4 :: _2 :: Nil, e#_2]
+        assertEquals(_4 :: _2 :: Nil, e._2)
+        meta.assertSame[_1 :: _5 :: Nil, r#next]
+        assertEquals(_1 :: _5 :: Nil, r.next)
+    }
+
+}
