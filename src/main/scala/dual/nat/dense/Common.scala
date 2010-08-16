048e13d2165d99c357d8bee62eaa0212e276e6da
unknown <okomok@gmail.com>
2010-08-16 22:57:01 +0900

    Ascii literals

--- src/main/scala/dual/nat/dense/Common.scala
+++ src/main/scala/dual/nat/dense/Common.scala
@@ -9,9 +9,9 @@ package dual; package nat; package dense
 
 
 private[dual]
-class Common extends LiteralCommon {
+class Common extends CommonLiteral {
     @returnThis
-    val Literal: LiteralCommon = this
+    val Literal: CommonLiteral = this
 
     @equivalentTo("new Nil{}")
     val Nil: Nil = _Dense.Nil
@@ -19,50 +19,4 @@ class Common extends LiteralCommon {
     @aliasOf("Cons")
      val :: = Cons
     type ::[x <: Boolean, xs <: Dense] = Cons[x, xs]
-}
-
-
-private[dual]
-trait LiteralCommon {
-    @aliasOf("`false`")
-     val _0B = `false`
-    type _0B = `false`
-
-    @aliasOf("`true`")
-     val _1B = `true`
-    type _1B = `true`
-
-	val  _0 = _Dense.Nil
-	val  _1 = Cons(_1B, _Dense.Nil)
-	val  _2 = Cons(_0B, Cons(_1B, _Dense.Nil))
-	val  _3 = Cons(_1B, Cons(_1B, _Dense.Nil))
-	val  _4 = Cons(_0B, Cons(_0B, Cons(_1B, _Dense.Nil)))
-	val  _5 = Cons(_1B, Cons(_0B, Cons(_1B, _Dense.Nil)))
-	val  _6 = Cons(_0B, Cons(_1B, Cons(_1B, _Dense.Nil)))
-	val  _7 = Cons(_1B, Cons(_1B, Cons(_1B, _Dense.Nil)))
-	val  _8 = Cons(_0B, Cons(_0B, Cons(_0B, Cons(_1B, _Dense.Nil))))
-	val  _9 = Cons(_1B, Cons(_0B, Cons(_0B, Cons(_1B, _Dense.Nil))))
-	val _10 = Cons(_0B, Cons(_1B, Cons(_0B, Cons(_1B, _Dense.Nil))))
-	val _11 = Cons(_1B, Cons(_1B, Cons(_0B, Cons(_1B, _Dense.Nil))))
-	val _12 = Cons(_0B, Cons(_0B, Cons(_1B, Cons(_1B, _Dense.Nil))))
-	val _13 = Cons(_1B, Cons(_0B, Cons(_1B, Cons(_1B, _Dense.Nil))))
-	val _14 = Cons(_0B, Cons(_1B, Cons(_1B, Cons(_1B, _Dense.Nil))))
-	val _15 = Cons(_1B, Cons(_1B, Cons(_1B, Cons(_1B, _Dense.Nil))))
-
-	type  _0 = Nil
-	type  _1 = Cons[_1B, Nil]
-	type  _2 = Cons[_0B, Cons[_1B, Nil]]
-	type  _3 = Cons[_1B, Cons[_1B, Nil]]
-	type  _4 = Cons[_0B, Cons[_0B, Cons[_1B, Nil]]]
-	type  _5 = Cons[_1B, Cons[_0B, Cons[_1B, Nil]]]
-	type  _6 = Cons[_0B, Cons[_1B, Cons[_1B, Nil]]]
-	type  _7 = Cons[_1B, Cons[_1B, Cons[_1B, Nil]]]
-	type  _8 = Cons[_0B, Cons[_0B, Cons[_0B, Cons[_1B, Nil]]]]
-	type  _9 = Cons[_1B, Cons[_0B, Cons[_0B, Cons[_1B, Nil]]]]
-	type _10 = Cons[_0B, Cons[_1B, Cons[_0B, Cons[_1B, Nil]]]]
-	type _11 = Cons[_1B, Cons[_1B, Cons[_0B, Cons[_1B, Nil]]]]
-	type _12 = Cons[_0B, Cons[_0B, Cons[_1B, Cons[_1B, Nil]]]]
-	type _13 = Cons[_1B, Cons[_0B, Cons[_1B, Cons[_1B, Nil]]]]
-	type _14 = Cons[_0B, Cons[_1B, Cons[_1B, Cons[_1B, Nil]]]]
-	type _15 = Cons[_1B, Cons[_1B, Cons[_1B, Cons[_1B, Nil]]]]
 }
--- src/main/scala/dual/nat/peano/Common.scala
+++ src/main/scala/dual/nat/peano/Common.scala
@@ -9,78 +9,10 @@ package dual; package nat; package peano
 
 
 private[dual]
-class Common extends LiteralCommon {
+class Common extends CommonLiteral {
     @returnThis
-    val Literal: LiteralCommon = this
+    val Literal: CommonLiteral = this
 
     @equivalentTo("new Zero{}")
     val Zero = _Peano.Zero
-}
-
-
-private[dual]
-trait LiteralCommon {
-    val  _0 = _Peano.Zero
-    val  _1 = Succ(_0)
-    val  _2 = Succ(_1)
-    val  _3 = Succ(_2)
-    val  _4 = Succ(_3)
-    val  _5 = Succ(_4)
-    val  _6 = Succ(_5)
-    val  _7 = Succ(_6)
-    val  _8 = Succ(_7)
-    val  _9 = Succ(_8)
-    val _10 = Succ(_9)
-    val _11 = Succ(_10)
-    val _12 = Succ(_11)
-    val _13 = Succ(_12)
-    val _14 = Succ(_13)
-    val _15 = Succ(_14)
-    val _16 = Succ(_15)
-    val _17 = Succ(_16)
-    val _18 = Succ(_17)
-    val _19 = Succ(_18)
-    val _20 = Succ(_19)
-    val _21 = Succ(_20)
-    val _22 = Succ(_21)
-    val _23 = Succ(_22)
-    val _24 = Succ(_23)
-    val _25 = Succ(_24)
-    val _26 = Succ(_25)
-    val _27 = Succ(_26)
-    val _28 = Succ(_27)
-    val _29 = Succ(_28)
-    val _30 = Succ(_29)
-
-    type  _0 = Zero
-    type  _1 = Succ[_0]
-    type  _2 = Succ[_1]
-    type  _3 = Succ[_2]
-    type  _4 = Succ[_3]
-    type  _5 = Succ[_4]
-    type  _6 = Succ[_5]
-    type  _7 = Succ[_6]
-    type  _8 = Succ[_7]
-    type  _9 = Succ[_8]
-    type _10 = Succ[_9]
-    type _11 = Succ[_10]
-    type _12 = Succ[_11]
-    type _13 = Succ[_12]
-    type _14 = Succ[_13]
-    type _15 = Succ[_14]
-    type _16 = Succ[_15]
-    type _17 = Succ[_16]
-    type _18 = Succ[_17]
-    type _19 = Succ[_18]
-    type _20 = Succ[_19]
-    type _21 = Succ[_20]
-    type _22 = Succ[_21]
-    type _23 = Succ[_22]
-    type _24 = Succ[_23]
-    type _25 = Succ[_24]
-    type _26 = Succ[_25]
-    type _27 = Succ[_26]
-    type _28 = Succ[_27]
-    type _29 = Succ[_28]
-    type _30 = Succ[_29]
 }
