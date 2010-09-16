248629240c614fbe7e5e375c083fb43f45e5adaf
unknown <okomok@gmail.com>
2010-09-16 15:06:42 +0900

    dust

---
+++ src/main/scala/sequence/reactive/Merge.scala
@@ -0,0 +1,17 @@
+
+
+// Copyright Shunsuke Sogame 2008-2010.
+// Distributed under the terms of an MIT-style license.
+
+
+package com.github.okomok.mada
+package sequence; package reactive
+
+
+private[reactive]
+case class Merge[+A](_1: Reactive[A], _2: Reactive[A]) extends Reactive[A] {
+    override def foreach(f: A => Unit) = {
+        for (x <- _1) f(x)
+        for (y <- _2) f(y)
+    }
+}
