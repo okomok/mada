7d0d4fbb87441ecaf7f64bfadb2449fdd4f67d7d
unknown <okomok@gmail.com>
2010-09-21 22:41:53 +0900

    thread-safe zip

--- src/main/scala/sequence/reactive/Zip.scala
+++ src/main/scala/sequence/reactive/Zip.scala
@@ -9,32 +9,35 @@ package sequence; package reactive
 
 
 import java.util.LinkedList
-import java.util.concurrent.ConcurrentLinkedQueue
 
 
-@notThreadSafe
 private
 case class Zip[A, B](_1: Reactive[A], _2: Reactive[B]) extends Reactive[Tuple2[A, B]] {
     override def foreach(f: Tuple2[A, B] => Unit) = {
         val q1 = new LinkedList[A]
         val q2 = new LinkedList[B]
+        val lock = new AnyRef
         def invariant() = assert(q1.isEmpty || q2.isEmpty)
 
         for (x <- _1) {
-            invariant
-            if (q2.isEmpty) {
-                q1.add(x)
-            } else {
-                f(x, q2.poll)
+            lock.synchronized {
+                invariant
+                if (q2.isEmpty) {
+                    q1.add(x)
+                } else {
+                    f(x, q2.poll)
+                }
             }
         }
 
         for (y <- _2) {
-            invariant()
-            if (q1.isEmpty) {
-                q2.add(y)
-            } else {
-                f(q1.poll, y)
+            lock.synchronized {
+                invariant()
+                if (q1.isEmpty) {
+                    q2.add(y)
+                } else {
+                    f(q1.poll, y)
+                }
             }
         }
     }
