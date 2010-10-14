0f38f8d7f2cb90282c52ceceaf66ba2eba92fa79
unknown <okomok@gmail.com>
2010-10-14 16:09:38 +0900

    much better test

--- src/test/scala/sequence/reactive/ShiftTest.scala
+++ src/test/scala/sequence/reactive/ShiftTest.scala
@@ -17,10 +17,10 @@ class ShiftTest extends org.scalatest.junit.JUnit3Suite {
 
     def testTrivial: Unit = {
         val s = new java.util.ArrayList[Int]
-        for (x <- reactive.Of(0,1,2,3,4).shift(k => {s.add(99);k()}).map(_+1)) {
+        for (x <- reactive.Of(0,1,2,3,4).shift(k => {s.add(99);k();k()}).map(_+1)) {
             s.add(x)
         }
-        assertEquals(vector.Of(99,1,99,2,99,3,99,4,99,5), vector.from(s))
+        assertEquals(vector.Of(99,1,1,99,2,2,99,3,3,99,4,4,99,5,5), vector.from(s))
     }
 
     def testSwing(off: Int) {
