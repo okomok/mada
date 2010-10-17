3f2f7850e72edcda1534864caa9ac71de9f1964a
unknown <okomok@gmail.com>
2010-10-17 16:29:46 +0900

    github maven woes

--- project/build/Mada.scala
+++ project/build/Mada.scala
@@ -11,4 +11,18 @@ class Mada(info: ProjectInfo) extends DefaultProject(info) {
     val junit = "junit" % "junit" % "3.8.2" % "test"
     val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
     override def compileOptions = Seq(Deprecation, Unchecked/*, ExplainTypes*/) ++ compileOptions("-Yrecursion", "50") ++ super.compileOptions
+
+    override def managedStyle = ManagedStyle.Maven
+    override def pomExtra =
+        <distributionManagement>
+            <repository>
+                <id>repo</id>
+                <url>http://github.com/okomok/maven-repo/raw/master/releases</url>
+            </repository>
+            <repository>
+                <id>snapshot-repo</id>
+                <url>http://github.com/okomok/maven-repo/raw/master/snapshots</url>
+            </repository>
+        </distributionManagement>
+    lazy val publishTo = Resolver.file("Publish", new java.io.File("../maven-repo/snapshots/"))
 }
