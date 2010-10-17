b1059d4bae91c57901763981d2eebdab7cf2aa47
unknown <okomok@gmail.com>
2010-10-17 12:42:03 +0900

    LiftTest comments.

--- src/main/scala/dual/Equality.scala
+++ src/main/scala/dual/Equality.scala
@@ -9,6 +9,16 @@ package dual
 
 
 /**
+ * Mixin for a metatype whose `equals` is reference-equality.
+ */
+trait ReferenceEquality extends Any {
+    override def equals(that: scala.Any) = refEquals(that)
+    override def hashCode = refHashCode
+    override def toString = refToString
+    override def canEqual(that: scala.Any): scala.Boolean = throw new UnsupportedOperationException("ReferenceEquality.canEqual")
+}
+
+/**
  * Mixin for a metatype whose `equals` is value-equality.
  */
 trait ValueEquality extends Any {
--- src/test/scala/dual/LiftTest.scala
+++ src/test/scala/dual/LiftTest.scala
@@ -21,6 +21,7 @@ class LiftTest extends org.scalatest.junit.JUnit3Suite {
 
     //val :: = mada.dual.list.Cons // oops
 
+    // Define dual `Function2`.
     object Apply extends Function2 {
         override type self = Apply.type
         override  def apply[f <: Any, x <: Any](f: f, x: x): apply[f, x] = f.asFunction1.apply(x)
@@ -28,11 +29,15 @@ class LiftTest extends org.scalatest.junit.JUnit3Suite {
     }
 
     def testTrivial {
+        // Normal types are not first-class citizen in dual world.
+        // `Box` turns them into dual ones. (No "autoboxing".)
         val y1 = Box(9.75) :: Box('x') :: Nil
         val y2 = Box(-2.125) :: Box('X') :: Nil
 
+        // `Lift1` too is a kind of boxing, which turns a normal function into dual one.
         val z = Lift1((_: Double) + .5) :: Lift1((_: Char).isUpper) :: Nil
 
+        // `zipBy` returns a view(unspecified type). `force` turns a view into a concrete list.
         val z1: Box[scala.Double] :: Box[scala.Boolean] :: Nil = z.zipBy(y1, Apply).force
         val Box(10.25) :: Box(false) :: Nil = z1
 
@@ -40,6 +45,7 @@ class LiftTest extends org.scalatest.junit.JUnit3Suite {
         val Box(-1.625) :: Box(true) :: Nil = z2
 
         {
+            // escape from the dual world using `undual`.
             val :: = scala.::
             val 10.25 :: false :: scala.Nil = z1.undual
             val -1.625 :: true :: scala.Nil = z2.undual
@@ -50,7 +56,7 @@ class LiftTest extends org.scalatest.junit.JUnit3Suite {
     def testTrivial2 {
         val y1 = Box(9.75) :: Box('x') :: Nil
         val z = Lift1((_: Double) + .5) :: Box(3) :: Nil
-        z.zipBy(y1, Apply)
+        z.zipBy(y1, Apply) // doesn't crash for now, because this is a view...
         ()
     }
 
