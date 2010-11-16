97d76838bfc06c13b4c62d1ed6a19b2e3d83ba21
unknown <okomok@gmail.com>
2010-11-16 09:10:03 +0900

    polymorphic -> monomorphic conversion

--- src/main/scala/eval/Strategy.scala
+++ src/main/scala/eval/Strategy.scala
@@ -12,3 +12,8 @@ trait Strategy {
     def apply[R](f: ByName[R]): Function0[R]
     def apply[R](f: => R): Function0[R] // needed: http://lampsvn.epfl.ch/trac/scala/ticket/3237
 }
+
+object Strategy {
+    implicit def _toFunction[R](from: Strategy): (=> R) => Function0[R] = from.apply[R]
+    implicit def _toFunctionUnit[R](from: Strategy): (=> R) => Unit = function.discard(from.apply[R])
+}
